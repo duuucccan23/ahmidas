7f00db2e292b52b96b1288c3649c4bb9d2a914c5
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-06-23 17:30:04 +0000

    Tweaked the output of complex matrices to by slightly more human readable.

--- trunk/src/SU3/Matrix/Matrix_j.cpp
+++ trunk/src/SU3/Matrix/Matrix_j.cpp
@@ -2,8 +2,44 @@
 
 std::ostream &SU3::operator<<(std::ostream &out, SU3::Matrix const &mat)
 {
-  out << mat.d_data[0] << "  " << mat.d_data [1] << "  " << mat.d_data[2] << "\n";
-  out << mat.d_data[3] << "  " << mat.d_data [4] << "  " << mat.d_data[5] << "\n";
-  out << mat.d_data[6] << "  " << mat.d_data [7] << "  " << mat.d_data[8] << std::endl;
+  out << "[ " << ((mat.d_data[0].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[0].real())
+              << ((mat.d_data[0].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[0].imag()) << " * i,   "
+              << ((mat.d_data[1].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[1].real()) 
+              << ((mat.d_data[1].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[1].imag()) << " * i,   "
+              << ((mat.d_data[2].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[2].real()) 
+              << ((mat.d_data[2].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[2].imag()) << " * i;\n"
+      
+              << ((mat.d_data[3].real() > 0) ? "    " : "  - ")
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[3].real())
+              << ((mat.d_data[3].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[3].imag()) << " * i,   "
+              << ((mat.d_data[4].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[4].real()) 
+              << ((mat.d_data[4].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[4].imag()) << " * i,   "
+              << ((mat.d_data[5].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[5].real()) 
+              << ((mat.d_data[5].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[5].imag()) << " * i;\n"
+              
+              << ((mat.d_data[6].real() > 0) ? "    " : "  - ")
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[6].real())
+              << ((mat.d_data[6].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[6].imag()) << " * i,   "
+              << ((mat.d_data[7].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[7].real()) 
+              << ((mat.d_data[7].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[7].imag()) << " * i,   "
+              << ((mat.d_data[8].real() > 0) ? "  " : "- ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[8].real()) 
+              << ((mat.d_data[8].imag() > 0) ? " + " : " - ") 
+              << std::setw(7) << std::scientific << std::setprecision(5) << std::abs(mat.d_data[8].imag()) << " * i   ]"
+      << std::endl;
   return out;
 }

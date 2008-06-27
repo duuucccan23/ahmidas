c0bb62d82adcab13df8481f6a4d7b414419732d7
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-06-27 09:07:41 +0000

    Implemented Core::hcComponent as a separate class, corrected interchanged template parameters.

--- trunk/src/Core/Component/Component.h
+++ trunk/src/Core/Component/Component.h
@@ -4,6 +4,9 @@
 namespace Core
 {
   template< typename Element, size_t L, size_t T, typename Atom >
+  class hcComponent;
+  
+  template< typename Element, size_t L, size_t T, typename Atom >
   class Component
   {
     Field< Element, L, T > &d_parent;
@@ -18,12 +21,25 @@ namespace Core
       iterator end();
 
       Atom &element(Core::SpaceTimeIndex *idx);
+      hcComponent< typename Element, size_t L, size_t T, typename Atom > const &dagger() const;
       
       #include "Component.operator"
   };
+  
+  class hcComponent
+  {
+    Component< Element, L, T, Atom > const &d_parent;
+    
+    hcComponent(Component< Element,  L,  T,  Atom > const &parent);
+    
+    public:
+      Component< Element, L, T, Atom > const &dagger() const;
+  };
+  
 }
 
 #include "Component.inlines"
 #include "Component.iterator.inlines"
+#include "hcComponent.inlines"
 
 #endif

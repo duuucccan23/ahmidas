da8eed7582cbb3d7819cc39af750089d05781582
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-06-30 14:27:17 +0000

    Reunitarization test part 1 (random complex 3x3 matrix unitarization test) done

--- test/reunit1.cpp
+++ test/reunit1.cpp
@@ -7,14 +7,20 @@ int main(int argc, char **argv)
   mat.reunitarize();
   SU3::hcMatrix hc = mat.dagger();
   mat.rightMultiply(hc);
-/*  std::cout << std::norm(mat(0,0)) << std::endl;
-  std::cout << std::norm(mat(0,1)) << std::endl;
-  std::cout << std::norm(mat(0,2)) << std::endl;
-  std::cout << std::norm(mat(1,0)) << std::endl;
-  std::cout << std::norm(mat(1,1)) << std::endl;
-  std::cout << std::norm(mat(1,2)) << std::endl;
-  std::cout << std::norm(mat(2,0)) << std::endl;
-  std::cout << std::norm(mat(2,1)) << std::endl;
-  std::cout << std::norm(mat(2,2)) << std::endl;*/
-  return 0;
+  bool c0 = (std::abs(std::norm(mat(0,0)) - 1) < 1e-12);
+  bool c1 = (std::abs(std::norm(mat(0,1)) - 0) < 1e-24);
+  bool c2 = (std::abs(std::norm(mat(0,2)) - 0) < 1e-24);
+  bool c3 = (std::abs(std::norm(mat(1,0)) - 0) < 1e-24);
+  bool c4 = (std::abs(std::norm(mat(1,1)) - 1) < 1e-12);
+  bool c5 = (std::abs(std::norm(mat(1,2)) - 0) < 1e-24);
+  bool c6 = (std::abs(std::norm(mat(2,0)) - 0) < 1e-24);
+  bool c7 = (std::abs(std::norm(mat(2,1)) - 0) < 1e-24);
+  bool c8 = (std::abs(std::norm(mat(2,2)) - 1) < 1e-12);
+  if (c0 & c1 & c2 & c3 & c4 & c5 & c6 & c7 & c8)
+  {
+    std::cout << "Reunitarization of a random complex 3x3 matrix produces an SU3 matrix.\n";
+    return 0;
+  }
+  std::cout << "Non-SU3 matrix produced when unitarizing a random complex 3x3 matrix.\n";
+  return 1;
 }

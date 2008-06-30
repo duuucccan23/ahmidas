7e759c67888ce0bce0e025ac258462d76ea18678
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-06-30 14:08:01 +0000

    Beginning adding of testing of reunitarization

--- test/reunit1.cpp
+++ test/reunit1.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <Core/Core.h>
+#include <SU3/Matrix/Matrix.h>
 
 int main(int argc, char **argv)
 {
-  
-  
+  SU3::Matrix mat = SU3::Matrix::random();
+  mat.reunitarize();
+  SU3::hcMatrix hc = mat.dagger();
+  mat.rightMultiply(hc);
+/*  std::cout << std::norm(mat(0,0)) << std::endl;
+  std::cout << std::norm(mat(0,1)) << std::endl;
+  std::cout << std::norm(mat(0,2)) << std::endl;
+  std::cout << std::norm(mat(1,0)) << std::endl;
+  std::cout << std::norm(mat(1,1)) << std::endl;
+  std::cout << std::norm(mat(1,2)) << std::endl;
+  std::cout << std::norm(mat(2,0)) << std::endl;
+  std::cout << std::norm(mat(2,1)) << std::endl;
+  std::cout << std::norm(mat(2,2)) << std::endl;*/
   return 0;
 }

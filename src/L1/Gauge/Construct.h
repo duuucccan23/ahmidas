808c4881481f67757a2d4c365457411055cdedd6
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-08-21 15:48:38 +0000

    First draft of a gauge staple, at this point mainly for use in factorizing smearing.

---
+++ src/L1/Gauge/Construct.h
@@ -0,0 +1,23 @@
+#ifndef GUARD_CONSTRUCT_H
+#define GUARD_CONSTRUCT_H
+
+#include <L0/Core/Component.h>
+#include <L0/Core/Core.h>
+#include <L0/Core/Buffer.h>
+#include <L0/Core/Field.h>
+#include <L0/QCD/Gauge.h>
+#include <L0/SU3/Matrix.h>
+
+namespace Gauge
+{
+  namespace Construct
+  {
+    template< size_t L, size_t T >
+    Core::Buffer< SU3::Matrix > staple(Core::Field< QCD::Gauge, L, T > &field, Core::SpaceTimeIndex towards, Core::Direction dirTo, Core::SpaceTimeIndex over, Core::Direction dirOver);
+  }
+}
+
+#include "Construct/Construct_staple.template"
+
+#endif
+

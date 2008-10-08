26667fb37d7832249e171b704d056196498b972d
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-08 16:38:34 +0000

    Weave even simpler, added localSize

--- trunk/test/weave.cpp
+++ trunk/test/weave.cpp
@@ -5,11 +5,15 @@
 int main(int argc, char **argv)
 {
   std::cout << "Weaving\n";
-  Base::Weave< 4, 4> myWeave = Base::Weave< 4, 4 >::instance();
+  Base::Weave< 4, 8> myWeave = Base::Weave< 4, 8 >::instance();
   std::cout << myWeave.localVolume() << std::endl;
   std::cout << myWeave.dim(Base::idx_X) << std::endl;
   std::cout << myWeave.dim(Base::idx_Y) << std::endl;
   std::cout << myWeave.dim(Base::idx_Z) << std::endl;
   std::cout << myWeave.dim(Base::idx_T) << std::endl;  
+  std::cout << myWeave.localSize(Base::idx_X) << std::endl;
+  std::cout << myWeave.localSize(Base::idx_Y) << std::endl;
+  std::cout << myWeave.localSize(Base::idx_Z) << std::endl;
+  std::cout << myWeave.localSize(Base::idx_T) << std::endl;  
   return 0;
 }

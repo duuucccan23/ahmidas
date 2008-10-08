e3fa47829c8ecc119002c14b10c2cd240d52b483
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-08 11:07:02 +0000

    Decided to move the architecture dependent stuff a level down.

---
+++ src/Arch/MPI/Base/Base.h
@@ -0,0 +1,50 @@
+#ifndef GUARD_BASE_H
+#define GUARD_BASE_H
+
+#include <algorithm>
+#include <cassert>
+#include <stdint.h>
+
+namespace Base
+{
+  // Definitions for clarity in refering to indices -- beware the inverted order...
+  enum SpaceTimeIndex
+  {
+    idx_X = 0,
+    idx_Y = 1,
+    idx_Z = 2,
+    idx_T = 3
+  };
+
+  // Definitions to be used in refering to directions for shifts
+  enum Direction
+  {
+    dir_DOWN = -1,
+    dir_UP = 1
+  };
+
+  enum ColourIndex
+  {
+    col_RED = 0,
+    col_GREEN = 1,
+    col_BLUE = 2
+  };
+
+  enum DiracIndex
+  {
+    gam_1 = 0,
+    gam_2 = 1,
+    gam_3 = 2,
+    gam_4 = 3
+  };
+
+  enum MpiTags
+  {
+    TAG_GAUGEFIELD,
+    TAG_FILE_DISTRIBUTION
+  };
+}
+
+#include "Base/Base.inlines"
+
+#endif

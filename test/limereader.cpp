d62f225fd07444d1bd3152e92d8a2a1076150f78
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-08 15:14:50 +0000

    Modified Field to work with Weave.

--- test/limereader.cpp
+++ test/limereader.cpp
@@ -4,11 +4,9 @@
 
 int main(int argc, char **argv)
 {
-  MPI::Init(argc, argv);
-  Base/IO::Lime::Reader limeTest(argv[1]);
+  Base::IO::Lime::Reader limeTest(argv[1]);
   // Something more intelligent should be done here, probably.
   // Maybe we should construct a special test file for LIME.
   // This particular file is in fact ILDG and needs more care.
-  MPI::Finalize();
   return 0;
 }

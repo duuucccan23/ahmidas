94594939ba007195ecdde2cfa1c063df9d30eb6b
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-08 11:43:12 +0000

    Moving back components of Base that are in fact architecture independent.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-#include <mpi.h>
-
-using namespace std;
 
 int main(int argc, char **argv)
 {
-  cout << "MPI is " << (MPI::Is_initialized() ? "" : "not ") << "initialized." << endl;
-  cout << "MPI is " << (MPI::Is_finalized() ? "" : "not ") << "finalized." << endl;
-  MPI::Init(argc, argv);
-  cout << "MPI is " << (MPI::Is_initialized() ? "" : "not ") << "initialized." << endl;
-  cout << "MPI is " << (MPI::Is_finalized() ? "" : "not ") << "finalized." << endl;
-  MPI::Finalize();
-  cout << "MPI is " << (MPI::Is_initialized() ? "" : "not ") << "initialized." << endl;
-  cout << "MPI is " << (MPI::Is_finalized() ? "" : "not ") << "finalized." << endl;
+  std::cout << "Dumb test code that should at least compile under all circumstances." << std::endl;
   return 0;
 }
 

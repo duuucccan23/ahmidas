065c9f7101d544fa65f5f6d4c413b9c8400f7061
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-09 15:29:54 +0000

    saving ILDG in rough form is now possible

--- trunk/arch/scalar/L0/Base/IO.h
+++ trunk/arch/scalar/L0/Base/IO.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <L0/Base/IO/Lime/Reader.h>
+#include <L0/Base/IO/Lime/Writer.h>
 
 // Below is necessary to prevent circular including when we make IO functions friends of field.
 namespace Core
@@ -17,8 +18,12 @@ namespace Base
   {
     template <typename Element, size_t L, size_t T >
     void loadILDG(Core::Field< Element, L, T > *field, std::string const &filename);
+
+    template <typename Element, size_t L, size_t T >
+    void saveILDG(Core::Field< Element, L, T > *field, std::string const &filename);
   }
 }
 #include "IO/loadILDG.template"
+#include "IO/saveILDG.template"
 
 #endif

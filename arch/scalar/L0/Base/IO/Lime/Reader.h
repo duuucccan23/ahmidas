209dc764a2935aebd20f9b368c1e9aef51f886ae
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-09 11:51:37 +0000

    Field reading implemented for scalar builds.

--- arch/scalar/L0/Base/IO/Lime/Reader.h
+++ arch/scalar/L0/Base/IO/Lime/Reader.h
@@ -34,43 +34,44 @@ namespace Base
         std::string const &d_filename;
         std::ifstream      d_in;
 
-        std::vector< std::string >    d_types;
-        std::vector< int32_t >        d_messages;
-        std::vector< uint64_t >       d_sizes;
-        std::vector< std::streampos > d_offsets;
-        std::vector< int16_t >        d_versions;
+        std::vector< std::string >    d_types; //
+        std::vector< int32_t >        d_messages; //
+        std::vector< uint64_t >       d_sizes; //Size of the data elements of records
+        std::vector< std::streampos > d_offsets; //Offsets of the data elements of records
+        std::vector< int16_t >        d_versions; //
 
-        size_t d_currentRecord;
-        bool d_fail;
-        bool d_messagesCorrect;
+        size_t d_currentRecord; //Index of current record
+        bool d_fail; //Fail state
+        bool d_messagesCorrect; //Begin and end bytes set correct?
 
         public:
           Reader(std::string const &filename);
 
           template< typename DataType >
-          void read(DataType *buffer, size_t elements);
+          void read(DataType *buffer, size_t elements); //Reads elements of type Datatype into variable buffer
 
-          void retrieveRecord(size_t const record);
-          void retrieveMessageAndRecord(size_t const message, size_t const record);
+          void retrieveRecord(size_t const record); //Go to record with index record
+          void retrieveMessageAndRecord(size_t const message, size_t const record); //Retrieve message index from record index
+          void retrieveRecord(std::string const &type); //Go to record with given type
 
-          void nextRecord();
-          void previousRecord();
+          void nextRecord(); //Go to next record
+          void previousRecord(); //Go to previous record
 
-          std::string const &filename() const;
-          size_t records() const;
-          size_t messages() const;
+          std::string const &filename() const; //Report filename of currently open file
+          size_t records() const; //List number of records
+          size_t messages() const; //Lists number of messages in the current record
 
-          size_t currentRecord() const;
-          size_t currentMessage() const;
-          size_t recordSize() const;
+          size_t currentRecord() const; //Gives the index of the current record
+          size_t currentMessage() const; //Gives the index of the current message
+          size_t recordSize() const; //Gives the size in bytes of the current record
 
-          size_t findRecord(std::string const &type) const;
-          std::string const &type() const;
-          int16_t const &version() const;
+          size_t findRecord(std::string const &type) const; //Find the record index matching a name
+          std::string const &type() const; //Lime type of currently opened record
+          int16_t const &version() const; //Lime version of currently opened file
 
-          bool good() const;
-          bool fail() const;
-          bool messagesCorrect() const;
+          bool good() const; //Is the reader ready to read?
+          bool fail() const; //Did something fail?
+          bool messagesCorrect() const; //Are begin and end bytes correctly used?
 
           std::streampos tellg();
           void seekg(std::streampos const offset, size_t const record);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <L0/Core/Field.h>
+#include <L0/QCD/Gauge.h>
+#include <L0/Base/IO.h>
 
 int main(int argc, char **argv)
 {
   std::cout << "Dumb test code that should at least compile under all circumstances." << std::endl;
+  Core::Field< QCD::Gauge, 8, 8 > myfield;
+  Base::IO::loadILDG(&myfield, "../test/conf.88");
+  std::cout << myfield[0][0];
+  myfield[0][0].reunitarize();
+  std::cout << myfield[0][0];
   return 0;
 }
 

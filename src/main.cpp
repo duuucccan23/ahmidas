90ab27682107482a7c2467dabfe0b3fa202b857b
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-09 14:37:08 +0000

    Added writing capability to reunit test.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,8 @@ int main(int argc, char **argv)
   std::cout << "Dumb test code that should at least compile under all circumstances." << std::endl;
   Core::Field< QCD::Gauge, 8, 8 > myfield;
   Base::IO::loadILDG(&myfield, "../test/conf.88");
-  std::cout << myfield[0][0] << "\n\n" << myfield[1][0];
   myfield[0][0].reunitarize();
-  std::cout << "\n\n" << myfield[0][0] << "\n\n" << myfield[1][0];
+  std::cout << myfield[0][0] << std::endl;
   return 0;
 }
 

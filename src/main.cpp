ef64e09d6dca585af52238a514cb22b71b31f120
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-10 14:16:25 +0000

    Implementation of (.template trick) field generation based on a component of another field, square and staple functionality. Detailed check of fieldshift and several small induced bugs fixed. Documentation for fieldshifting added.

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,10 @@ int main(int argc, char **argv)
 {
   Core::Field< QCD::Gauge, 8, 8 > myfield;
   Base::IO::loadILDG(&myfield, "../test/conf.88");
-  Core::Field< SU3::Matrix, 8, 8 > result(myfield.component< SU3::Matrix >(Base::idx_Z));
   Core::Field< SU3::Matrix, 8, 8 > mystaple = Path::staple(myfield, Base::idx_X, Base::dir_UP, Base::idx_Y, Base::dir_UP);
+  std::cout << myfield[1][Base::idx_Y];
+  myfield.shift(Base::idx_X, Base::dir_DOWN);
+  std::cout << myfield[0][Base::idx_Y];
   return 0;
 }
 

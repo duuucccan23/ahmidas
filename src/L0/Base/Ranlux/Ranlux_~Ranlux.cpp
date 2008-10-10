6d4da69b0385bcdb62221497a20dfcda5d687eff
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-10 20:05:10 +0000

    Added an additional interface to the random number generators.

--- src/L0/Base/Ranlux/Ranlux_~Ranlux.cpp
+++
@@ -1,5 +0,0 @@
-#include "Ranlux.ih"
-
-Base::Ranlux::~Ranlux()
-{
-}
--- test/reunit.cpp
+++ test/reunit.cpp
@@ -7,8 +7,9 @@ int main(int argc, char **argv)
   std::ofstream *out = 0;
   if (argc > 1)
     out = new std::ofstream(argv[1]);
-  
-  for (size_t ctr = 0; ctr < 5000; ++ctr)
+
+  bool problem = false;
+  for (size_t ctr = 0; ctr < 300000; ++ctr)
   {
     SU3::Matrix mat = SU3::Matrix::random();
     mat.reunitarize();
@@ -17,18 +18,14 @@ int main(int argc, char **argv)
     double error = 0.0;
     for (size_t idx_x = 0; idx_x < 3; ++idx_x)
       for (size_t idx_y = 0; idx_y < 3; ++idx_y)
-	error = std::max((idx_x == idx_y) ? std::abs(1.0 - mat(idx_x, idx_y)) : std::abs(mat(idx_x, idx_y)), error);
+	  error = std::max((idx_x == idx_y) ? std::abs(1.0 - mat(idx_x, idx_y)) : std::abs(mat(idx_x, idx_y)), error);
     if (out)
       (*out) << error;
     if (std::abs(error) > 5e-10)
     {
       std::cout << "At matrix number " << ctr << ':' << std::endl;
       std::cout << "Stress test failed on unitarity with total deviation " << error << '!' << std::endl;
-      if (out)
-      {
-	out->close();
-	delete out;
-      }
+      problem = true;
     }
     
     SU3::Matrix copy(mat);
@@ -37,19 +34,14 @@ int main(int argc, char **argv)
     error = 0.0;
     for (size_t idx_x = 0; idx_x < 3; ++idx_x)
       for (size_t idx_y = 0; idx_y < 3; ++idx_y)
-	error += std::abs(mat(idx_x, idx_y) - copy(idx_x, idx_y));
+	  error += std::abs(mat(idx_x, idx_y) - copy(idx_x, idx_y));
     if (out)
       (*out) << "\t" << error << std::endl;
     if (std::abs(error) > 4e-15)
     {
       std::cout << "At matrix number " << ctr << ':' << std::endl;
       std::cout << "Stress test failed on double reunitarization with total deviation " << error << '!' << std::endl;
-      if (out)
-      {
-	out->close();
-	delete out;
-      }
-      return 1;
+      problem = true;
     }
   }
 
@@ -58,6 +50,9 @@ int main(int argc, char **argv)
     out->close();
     delete out;
   }
+
+  if (problem)
+    return 1;
   
   std::cout << "All matrices properly reunitarized in stress test." << std::endl;
   return 0;

3db4fe40891e2e399b37b9bbceae2606d4f0dfd5
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-11 09:58:15 +0000

    Added what in some cases amounts to placeholder code for the different sources we're going to need.

---
+++ trunk/src/L1/Source/Stochastic.h
@@ -0,0 +1,18 @@
+#ifndef GUARD_SOURCE_STOCHASTIC_H
+#define GUARD_SOURCE_STOCHASTIC_H
+
+#include <L0/Core/Field.h>
+
+namespace Source
+{
+  template< size_t L, size_t T >
+  class Stochastic
+  {
+    Field< double, L, 1 >  d_source;
+
+    public:
+      Stochastic();
+  };
+}
+
+#endif
---
+++ trunk/src/L1/Source/Wall.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_SOURCE_WALL_H
+#define GUARD_SOURCE_WALL_H
+
+namespace Source
+{
+  template< size_t L, size_t T >
+  class Wall
+  {};
+}
+
+#endif

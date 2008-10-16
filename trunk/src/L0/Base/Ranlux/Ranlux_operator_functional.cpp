fadc33ea8472cecea4215e61df80c33a88ddda48
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-16 18:52:07 +0000

    Added an additional, much faster random number generator. Based on the subtractive method of Knuth, described as ran3 in Numerical Recipes.

--- trunk/src/L0/Base/Ranlux/Ranlux_operator_functional.cpp
+++ trunk/src/L0/Base/Ranlux/Ranlux_operator_functional.cpp
@@ -4,8 +4,8 @@ double Base::Ranlux::operator()()
 {
   for (size_t ctr = 0; ctr < d_lux; ++ctr)
   {
-    ++d_index;
-    d_index %= d_period;
+    if (++d_index == d_period)
+      d_index = 0;
     d_state[d_index] -= d_state[(d_index + d_delta_s) % d_period] + d_carry;
     if (d_state[d_index] < 0.0)
     {

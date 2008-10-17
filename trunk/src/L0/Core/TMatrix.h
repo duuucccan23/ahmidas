ee1947c7cc8895ed035736a857cabe55a4c62e11
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-17 12:39:23 +0000

    Added a transfer matrix class TMatrix to store a time ordered array of tensors. Made modification to Tensor so that it will fulfill the needs of the type of Wick contraction we now envision. Started some updating on the Propagator class.

---
+++ trunk/src/L0/Core/TMatrix.h
@@ -0,0 +1,34 @@
+#ifndef GUARD_CORE_TMATRIX_H
+#define GUARD_CORE_TMATRIX_H
+
+#include <L0/QCD/Tensor.h>
+
+namespace Core
+{
+  template< size_t T >
+  class TMatrix
+  {
+    size_t      *d_references;
+    QCD::Tensor *d_transfer;
+
+    public:
+      TMatrix();
+      TMatrix(QCD::Tensor *transfer);
+      TMatrix(TMatrix const &other);
+      TMatrix &operator=(TMatrix const &other);
+      
+      ~TMatrix();
+
+    private:
+      void destroy();
+      void isolate();
+  }
+}
+
+#include "TMatrix/TMatrix.inlines"
+
+#include "TMatrix/TMatrix_destroy.template"
+#include "TMatrix/TMatrix_isolate.template"
+#include "TMatrix/TMatrix_operator_assign.template"
+
+#endif
--- trunk/src/L0/QCD/Tensor.h
+++ trunk/src/L0/QCD/Tensor.h
@@ -2,19 +2,26 @@
 #define GUARD_QCD_TENSOR_H
 
 #include <complex>
+
+#include <L0/Base/Base.h>
 #include <L0/Core/Core.h>
 
 namespace QCD
 {
   class Tensor
   {
-    size_t                 *d_references;
-    std::complex< double > *d_data;
+    std::complex< double > d_data[144];
 
     public:
       Tensor();
       Tensor(Tensor const &other);
       Tensor(std::complex< double > *data);
+
+      std::complex &operator[](size_t const idx);
+      std::complex const &operator[](size_t const idx) const;
+
+      std::complex &operator()(size_t const dirSink, size_t const colSink, size_t const dirSource, size_t const colSource);
+      std::complex const &operator()(size_t const dirSink, size_t const colSink, size_t const dirSource, size_t const colSource) const;
   };
 }
 

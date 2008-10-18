0881840a1f253e0f749aafcf1652772a955c72c8
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-18 20:29:36 +0000

    Final preparatory work - close to having implemented a pion contraction now.

--- src/L1/Tool.h
+++ src/L1/Tool.h
@@ -13,7 +13,12 @@ namespace Tool
 
   template< typename Element, size_t L, size_t T >
   void randomize(Core::Field< Element, L, T > *field);
+
+  template< size_t L, size_t T >
+  void randomize(Core::Propagator< L, T > *propagator);
 }
+
+#include "Tool/Tool.inlines"
 
 #include "Tool/Tool_reunitarize.template"
 #include "Tool/Tool_randomize.template"
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-#include <L0/Core/Correlator.h>
-#include <L0/Core/Field.h>
-#include <L0/QCD/Gauge.h>
-#include <L0/Base/IO.h>
-#include <L1/Source/Point.h>
+#include <L0/Core/Propagator.h>
 #include <L1/Tool.h>
-#include <L2/Contract/Wick.h>
 
 int main(int argc, char **argv)
 {
-  Core::Field< QCD::Spinor, 8, 16 > myfield;
-  Source::Point< 8, 16 > mysource(1, 1, 1);
-  Tool::randomize(&myfield);
-  Core::Correlator< 8, 16 > mycorr = Contract::wick(mysource, Base::gam_1, Base::col_RED, myfield);
-  std::cout << "Result of correlator contraction of point sink on random spinor field:" << std::endl;
-  for (size_t idx = 0; idx < 16; ++idx)
-    std::cout << "T = " << idx << "\t" << mycorr[idx] << std::endl;
+  Core::Propagator< 12, 8 > myprop;
+  Tool::randomize(&myprop);
+  
   return 0;
 }

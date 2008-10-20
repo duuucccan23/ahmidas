6598ba5451e2a2a3ff403e5ffc61ad41aab413a7
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-20 22:42:35 +0000

    Further rewrite of multiplication, include hermitian conjugate tensors. Also added calculation of a trace.

---
+++ src/L0/QCD/Tensor/Tensor_leftMultiply_b.cpp
@@ -0,0 +1,13 @@
+#include "Tensor.ih"
+
+QCD::Tensor &QCD::Tensor::leftMultiply(QCD::hcTensor const &other)
+{ 
+  std::complex< double > result[144];
+  std::fill_n(result, 144, std::complex< double >(0.0, 0.0)); // Default construction is not sufficient...
+  for (size_t rowIdx = 0; rowIdx < 12; ++rowIdx)
+    for (size_t colIdx = 0; colIdx < 12; ++colIdx)
+      for (size_t resIdx = 0; resIdx < 12; ++resIdx)
+        result[rowIdx + colIdx * 12] += other[resIdx + 12 * colIdx] * d_data[rowIdx + 12 * resIdx];
+  std::copy(result, result + 144, d_data);
+  return *this;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,14 @@ int main(int argc, char **argv)
   Base::IO::loadScidac(&myspinor, "../test/inv.2448");
 
   QCD::Tensor myTens;
-  QCD::Tensor otherTens;
+  for (size_t idx = 0; idx < 144; ++idx)
+    myTens[idx] = std::complex< double >(idx % 12, idx / 12);
+  myTens.leftMultiply(myTens.dagger());
   for (size_t idx = 0; idx < 144; ++idx)
   {
-    myTens[idx] = std::complex< double >(idx / 12, 0);
-    otherTens[idx] = std::complex< double >(0, idx / 12);
-  }
-  myTens.rightMultiply(otherTens);
-  for (size_t idx = 0; idx < 144; ++idx)
-  {
-    if (idx && idx % 12 == 0)
+    if (idx != 0 && ((idx % 12) == 0))
       std::cout << std::endl;
-    std::cout << myTens[idx] << "\t";
+    std::cout << myTens[idx] << " ";
   }
   std::cout << std::endl;
   return 0;

9faca289070293022e34d7a8b70f251e27045cdc
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-10-31 14:51:09 +0000

    Stochastic source rewritten to SU3::Vector from Core::Field, induced changes and preparation in Weave for localSpatialVolume.

--- trunk/src/L1/Source/Stochastic.h
+++ trunk/src/L1/Source/Stochastic.h
@@ -1,7 +1,7 @@
 #ifndef GUARD_SOURCE_STOCHASTIC_H
 #define GUARD_SOURCE_STOCHASTIC_H
 
-#include <L0/Core/Field.h>
+#include <L0/Base/Weave.h>
 #include <L0/SU3/Vector.h>
 #include <L0/Base/Random.h>
 #include <cmath>
@@ -11,13 +11,13 @@ namespace Source
   template< size_t L, size_t T >
   class Stochastic
   {
-    size_t const                      d_timeSlice;
-    Core::Field< SU3::Vector, L, 1 >  d_source;
+    SU3::Vector *d_source;
 
     public:
       Stochastic();
-      size_t timeSlice() const;
-      double const *source() const;
+      ~Stochastic();
+
+      SU3::Vector const *source() const;
   };
 }
 

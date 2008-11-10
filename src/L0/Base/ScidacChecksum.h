02de76d0ce5af59e40c37f353256c554b4f8f896
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-10 12:32:41 +0000

    Checksum should now be fully implemented, but awaiting compiling and testing.

--- src/L0/Base/ScidacChecksum.h
+++ src/L0/Base/ScidacChecksum.h
@@ -8,24 +8,36 @@ namespace Base
 {
   class ScidacChecksum
   {
-    uint32_t d_sum29;
-    uint32_t d_sum31;
+    union
+    {
+      uint32_t as32[2];
+      uint64_t as64;
+    } d_sum;
 
     uint32_t d_crcTable[256];
 
     public:
       ScidacChecksum();
       void clear();
+      uint64_t checksum();
 
       template< typename Element >
-      void aggregate(Element *data, size_t elements, size_t rank = 0);
+      size_t aggregate(Element *data, size_t elements, size_t rank = 0);
 
     private:
       void createTable();
+
+      template< typename Element >
+      uint32_t crc32(Element &buffer);
+
+      template< typename Element >
+      uint32_t crc32(Element *buffer, size_t length = 1);
   };
 }
 
 #include "ScidacChecksum/ScidacChecksum.inlines"
-//#include "ScidacChecksum/ScidacChecksum_aggregate.template"
+#include "ScidacChecksum/ScidacChecksum_aggregate.template"
+#include "ScidacChecksum/ScidacChecksum_crc32_a.template"
+#include "ScidacChecksum/ScidacChecksum_crc32_b.template"
 
 #endif

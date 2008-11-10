79be38c6e2063c6ecf8ce7ec464beda1cbc77d90
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-10 11:11:36 +0000

    Forgot to add header file.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,23 +1,12 @@
 #include <iostream>
+#include <iomanip>
 
-#include <L0/Base/Base.h>
-#include <L0/Base/IO.h>
-#include <L0/Base/Random.h>
-#include <L0/Core/Field.h>
-#include <L0/Core/TMatrix.h>
-#include <L0/QCD/Spinor.h>
-#include <L0/QCD/Tensor.h>
-#include <L1/Source/Stochastic.h>
-#include <L1/Source/Full.h>
-#include <L1/Tool.h>
-#include <L1/Tool/IO.h>
+#include <L0/Base/ScidacChecksum.h>
 
 int main(int argc, char **argv)
 {
-  Core::Field< QCD::Spinor, 24, 48 > testspinor = Base::IO::loadScidac< QCD::Spinor, 24, 48 >("../test/smearsource0.2448");
-  std::cout << testspinor[0][0];
-  Base::IO::saveILDG(testspinor, "../test/testspinor.2448");
-  Source::Full< 8, 8 > mysource = Source::Full< 8, 8 >();
-  Tool::IO::saveScidac(mysource, "../test/fulltest.88");
+  Base::ScidacChecksum check;
+  for (size_t idx = 0; idx < 256; ++idx)
+    std::cout << std::dec << idx << "  " << std::hex << check.d_crcTable[idx] << std::endl;
   return 0;
 }

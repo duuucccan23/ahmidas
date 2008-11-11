e0172edd88dc91a58c6bbb6f2d469752ebef2e63
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-11 15:27:50 +0000

    Scidac checksums fully implemented, main file changed to something that tests the new code (and properly compiles). We still need the -std=c++0x, because I smuggled in a little static_assert call just for kicks. Should be replaced at a later stage, but it works like a charm for now.

--- arch/scalar/L1/Tool/IO.h
+++ arch/scalar/L1/Tool/IO.h
@@ -1,18 +1,22 @@
 #ifndef GUARD_TOOL_IO_H
 #define GUARD_TOOL_IO_H
 
-#include <string>
-#include <L1/Source/Stochastic.h>
-#include <L0/Base/IO/Lime/Writer.h>
-#include <sstream>
+#include <cassert>
 #include <iomanip>
+#include <sstream>
+#include <string>
+
+#include <L0/Base/IO/Lime/Writer.h>
+#include <L0/Base/ScidacChecksum.h>
+#include <L1/Source/Full.h>
+#include <L1/Source/Stochastic.h>
 
 namespace Tool
 {
   namespace IO
   {
     template< size_t L, size_t T >
-    void saveScidac(Source::Stochastic< L, T > const &source, std::string const &basefilename, size_t timeslice);
+    void saveScidac(Source::Stochastic< L, T > const &source, std::string const &basefilename, size_t timeIdx);
 
     template< size_t L, size_t T >
     void saveScidac(Source::Full< L, T > &source, std::string const &basefilename);

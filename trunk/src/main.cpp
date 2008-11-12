df0f182e8a74db41ec6cc449f7910464dad9ced8
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-12 15:37:21 +0000

    Commit of broken main.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -3,28 +3,15 @@
 #include <sstream>
 
 #include <L0/Tool/IO.h>
-#include <L0/Base/Random.h>
 #include <L0/Core/Field.h>
-#include <L0/Core/TMatrix.h>
 #include <L0/QCD/Gauge.h>
-#include <L0/QCD/Spinor.h>
-#include <L0/QCD/Tensor.h>
-#include <L1/Tool.h>
+#include <L1/Path.h>
 
 int main(int argc, char **argv)
 {
-  Core::Field<QCD::Gauge, 4, 4 > field = Tool::IO::loadILDG<QCD::Gauge, 4, 4 >(std::string("../test/lat.sample.l4444.ildg"));
-  Tool::reunitarize(&field);
-  std::cerr << field[0][0] << std::endl;
-
-  Core::Field<QCD::Gauge, 4, 4 > sfield = Tool::IO::loadMILC<QCD::Gauge, 4, 4 >(std::string("../test/lat.sample.l4444"));
-  Tool::reunitarize(&sfield);
-  std::cerr << sfield[0][0] << std::endl;
-  std::cerr << det(sfield[0][0]) << std::endl;
-
-  Core::Field< QCD::Gauge, 8, 8 > qField = Tool::IO::loadILDG< QCD::Gauge, 8, 8 >(std::string("../test/conf.88"));
-  Tool::reunitarize(&field);
-  std::cerr << qField[0][0] << std::endl;
+  Core::Field<QCD::Gauge, 6, 6 > field = Tool::IO::loadMILC<QCD::Gauge, 6, 6 >(std::string("../test/lat.nf8_m02_12x6_b3600_1"));
+  Core::Field<SU3::Matrix, 6, 6 > square = Path::square(field, Base::idx_T, Base::dir_UP, Base::idx_X, Base::dir_UP);
+  std::cout << square[4] << std::endl;
 
   return 0;
 }

b04fefb89082c819d30572c1f6dcb732a068f66d
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-19 11:15:32 +0000

    Added gauge fixing.

--- trunk/src/L1/Tool.h
+++ trunk/src/L1/Tool.h
@@ -6,21 +6,42 @@
 #include <L0/Core/Propagator.h>
 #include <L0/QCD/Gauge.h>
 #include <L0/SU3/Matrix.h>
+#include <L1/Path.h>
 
 namespace Tool
 {
   template< size_t L, size_t T >
   void reunitarize(Core::Field< QCD::Gauge, L, T > *field);
 
+  template< size_t L, size_t T >
+  void reunitarize(Core::Field< SU3::Matrix, L, T > *field);
+
   template< typename Element, size_t L, size_t T >
   void randomize(Core::Field< Element, L, T > *field);
 
   template< size_t L, size_t T >
   void randomize(Core::Propagator< L, T > *propagator);
-  
+
   template< typename Element, size_t L, size_t T >
   void setToZero(Core::Field< Element, L, T > *field);
 
+  template< size_t L, size_t T >
+  void setToIdentity(Core::Field< SU3::Matrix, L, T > *field);
+
+  template< typename Element, size_t L, size_t T >
+  std::complex< double > tr(Core::Field< Element, L, T > const &field);
+
+  template< size_t L, size_t T >
+  Core::Field< SU3::Matrix, L, T > localTrace(Core::Field< SU3::Matrix, L, T > const &field);
+
+  template< size_t L, size_t T >
+  std::complex< double > spatialPlaquette(Core::Field< QCD::Gauge, L, T > &field);
+
+  template< size_t L, size_t T >
+  std::complex< double > temporalPlaquette(Core::Field< QCD::Gauge, L, T > &field);
+
+  template< size_t L, size_t T >
+  void fixCoulombGauge(Core::Field< QCD::Gauge, L, T > *field);
 }
 
 #include "Tool/Tool.inlines"
@@ -28,5 +49,9 @@ namespace Tool
 #include "Tool/Tool_reunitarize.template"
 #include "Tool/Tool_randomize.template"
 #include "Tool/Tool_setToZero.template"
+#include "Tool/Tool_localTrace.template"
+#include "Tool/Tool_spatialPlaquette.template"
+#include "Tool/Tool_temporalPlaquette.template"
+#include "Tool/Tool_fixCoulombGauge.template"
 
 #endif
--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,17 +1,22 @@
 #include <iomanip>
 #include <iostream>
+#include <fstream>
 #include <sstream>
 
 #include <L0/Tool/IO.h>
+#include <L0/Core/Component.h>
 #include <L0/Core/Field.h>
 #include <L0/QCD/Gauge.h>
 #include <L1/Path.h>
+#include <L1/Tool.h>
+#include <L1/Smear/APE.h>
 
 int main(int argc, char **argv)
 {
-  Core::Field<QCD::Gauge, 6, 6 > field = Tool::IO::loadMILC<QCD::Gauge, 6, 6 >(std::string("../test/lat.nf8_m02_12x6_b3600_1"));
-  Core::Field<SU3::Matrix, 6, 6 > square = Path::square(field, Base::idx_T, Base::dir_UP, Base::idx_X, Base::dir_UP);
-  std::cout << square[4] << std::endl;
+  Core::Field<QCD::Gauge, 12, 4 > field =
+      Tool::IO::loadMILC<QCD::Gauge, 12, 4 >("../test/lat.nf8_m02_12x6_b3600_1");
+
+  Tool::fixCoulombGauge(&field);
 
   return 0;
 }

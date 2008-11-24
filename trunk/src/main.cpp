27ffd4a2a24a0e69ec291be99e0d17ff003c0287
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-24 00:51:49 +0000

    Minor change, what can be done in operator_lshift for SU3::Vector can also be done for SU3::Matrix

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -5,11 +5,11 @@
 
 #include <L0/Tool/IO.h>
 #include <L0/Core/Field.h>
-#include <L0/QCD/Spinor.h>
+#include <L0/QCD/Gauge.h>
 
 int main(int argc, char **argv)
 {
-  Core::Field< QCD::Spinor, 4, 4 > myfield = Tool::IO::loadScidac<QCD::Spinor, 4, 4 >("../test/AHMSource.0.00");
+  Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../test/conf.88");
   
   std::cout << myfield[0][0];
   return 0;

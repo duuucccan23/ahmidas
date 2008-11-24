40f3c69eec5574588c846fdafc213bc86f3d1084
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-24 20:46:00 +0000

    Preparation of Path::step for multistep smearings such as Fuzz

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -6,11 +6,23 @@
 #include <L0/Tool/IO.h>
 #include <L0/Core/Field.h>
 #include <L0/QCD/Gauge.h>
+#include <L1/Smear/APE.h>
 
 int main(int argc, char **argv)
 {
   Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../test/conf.88");
   
-  std::cout << myfield[0][0];
+  SU3::Matrix mat1(myfield[0][Base::idx_X]);
+  SU3::Matrix mat2(myfield[1][Base::idx_X]);
+  SU3::Matrix mat3(myfield[2][Base::idx_X]);
+  mat3.leftMultiply(mat2);
+  mat3.leftMultiply(mat1);
+  std::cout << mat3;
+  Core::Field< SU3::Matrix, 8, 8 > mysteps = Path::step(myfield, Base::idx_X, Base::dir_UP, 3);
+  mysteps.shift(Base::idx_X, Base::dir_DOWN);
+  mysteps.shift(Base::idx_X, Base::dir_DOWN);
+  mysteps.shift(Base::idx_X, Base::dir_DOWN);
+  std::cout << mysteps[0];
+  
   return 0;
 }

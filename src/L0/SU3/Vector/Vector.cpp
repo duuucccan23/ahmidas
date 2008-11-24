57b803c116404beeea0e3ca888637584c167e6b9
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2008-11-24 11:26:37 +0000

    More cleanup of Matrix and Vector.

--- src/L0/SU3/Vector/Vector.cpp
+++ src/L0/SU3/Vector/Vector.cpp
@@ -2,9 +2,12 @@
 
 namespace
 {
-  double zero[]    = { 0.0, 0.0,
-                       0.0, 0.0,
-                       0.0, 0.0 };
+  double const zero[] =
+  {
+    0.0, 0.0,
+    0.0, 0.0,
+    0.0, 0.0
+  };
 }
 
 SU3::Vector const SU3::Vector::s_zero(::zero);

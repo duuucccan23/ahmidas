35108b321a05a07c336ebfbc50e035ab05e6d372
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2009-01-07 11:12:41 +0000

    Trying to get properties added in SVN

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,3 +1,4 @@
+// $Id$
 #include <iomanip>
 #include <iostream>
 #include <fstream>
@@ -10,19 +11,8 @@
 
 int main(int argc, char **argv)
 {
-  Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../test/conf.88");
-  
-  SU3::Matrix mat1(myfield[0][Base::idx_X]);
-  SU3::Matrix mat2(myfield[1][Base::idx_X]);
-  SU3::Matrix mat3(myfield[2][Base::idx_X]);
-  mat3.leftMultiply(mat2);
-  mat3.leftMultiply(mat1);
-  std::cout << mat3;
-  Core::Field< SU3::Matrix, 8, 8 > mysteps = Path::step(myfield, Base::idx_X, Base::dir_UP, 3);
-  mysteps.shift(Base::idx_X, Base::dir_DOWN);
-  mysteps.shift(Base::idx_X, Base::dir_DOWN);
-  mysteps.shift(Base::idx_X, Base::dir_DOWN);
-  std::cout << mysteps[0];
+  Core::Field< QCD::Gauge, 20, 48 > myfield = Tool::IO::loadILDG<QCD::Gauge, 20, 48 >("../test/conf.2448");
+  Tool::IO::saveILDG(myfield, "../test/conf.2048");
   
   return 0;
 }

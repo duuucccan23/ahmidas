95a9351579644de1a5948687654310ceb90b55ba
dinter <dinter@ifh.de>
2009-09-22 16:48:38 +0000

    added Propagator::view to acess lower dimensional part of Propagator

--- src/L0/Core/Propagator.h
+++ src/L0/Core/Propagator.h
@@ -6,17 +6,17 @@
 
 namespace Core
 {
-  // NOTE Dirac is the  n u m b e r  of Dirac indices, not a particualar Dirac index
-  // same is true for Colour 
-  template< size_t L, size_t T, size_t Dirac, size_t Colour >
+  // NOTE nDirac is the  n u m b e r  of Dirac indices, not a particualar Dirac index
+  // same is true for nColour , being the number of colour indices
+  template< size_t L, size_t T, size_t nDirac, size_t nColour >
   class Propagator
   {
     size_t *d_references;
 
-    Core::Field< std::complex< double >, L, T > *d_components;
+    Core::Field< std::complex< double >, L, T > *const *d_components;
 
-    size_t *colour_strides;
-    size_t *dirac_strides;
+    size_t **colour_strides;
+    size_t **dirac_strides;
 
     public:
       Propagator();
@@ -25,45 +25,44 @@ namespace Core
 
 #include "Propagator/Propagator.operators"
 
-#include "Propagator/Propagator.iterator"
-#include "Propagator/Propagator.const_iterator"
+#include "Propagator/Propagator.view"
+// #include "Propagator/Propagator.const_view"
 
-      iterator_full begin();
-      iterator_full end();
+// #include "Propagator/Propagator.iterator"
+// #include "Propagator/Propagator.const_iterator"
 
-      const_iterator_full begin() const;
-      const_iterator_full end() const;
+//       iterator begin(Base::ColourIndex const idx, const size_t ColourID);
+//       iterator end(Base::ColourIndex const idx, const size_t ColourID);
+//       iterator begin(Base::DiracIndex  const idx, const size_t DiracID);
+//       iterator end(Base::DiracIndex  const idx, const size_t DiracID);
 
-      iterator_colour begin(Base::ColourIndex const idx, const size_t ColourID);
-      iterator_colour end(Base::ColourIndex const idx, const size_t ColourID);
+//       const_iterator begin(Base::ColourIndex const idx, const size_t ColourID) const;
+//       const_iterator end(Base::ColourIndex const idx, const size_t ColourID) const;// 
+//       const_iterator begin(Base::DiracIndex const idx, const size_t DiracID) const;
+//       const_iterator end(Base::DiracIndex const idx, const size_t DiracID) const;
 
-      const_iterator_colour begin(Base::ColourIndex const idx, const size_t ColourID) const;
-      const_iterator_colour end(Base::ColourIndex const idx, const size_t ColourID) const;
-
-      iterator_dirac begin(Base::DiracIndex const idx, const size_t DiracID);
-      iterator_dirac end(Base::DiracIndex const idx, const size_t DiracID);
-
-      const_iterator_dirac begin(Base::DiracIndex const idx, const size_t DiracID) const;
-      const_iterator_dirac end(Base::DiracIndex const idx, const size_t DiracID) const;
-
-      Propagator< L, T, Dirac-1, Colour > &getDirac(Base::DiracIndex const dirIdx, const size_t DiracID);
-      Propagator< L, T, Dirac-1, Colour > const &getDirac(Base::DiracIndex const dirIdx, const size_t DiracID) const;
-
-      Propagator< L, T, Dirac, Colour-1 > &getColour(Base::ColourIndex const dirIdx, const size_t ColourID);
-      Propagator< L, T, Dirac, Colour-1 > const &getColour(Base::ColourIndex const dirIdx, const size_t ColourID) const;
+      view< L, T, nDirac-1, nColour > &operator()(Base::DiracIndex const idx, const size_t DiracID);
+      view< L, T, nDirac, nColour-1 > &operator()(Base::ColourIndex const idx, const size_t ColourID);
+      
+//       const_view< L, T, nDirac-1, nColour > const &operator()(Base::DiracIndex const idx, const size_t DiracID) const;      
+//       const_view< L, T, nDirac, nColour-1 > const &operator()(Base::ColourIndex const idx, const size_t ColourID) const;
+      
 
       size_t size() const;
 
     private:
       void destroy();
       void isolate();
-      void getStrides();
   };
 }
 
 #include "Propagator/Propagator.inlines"
-#include "Propagator/Propagator.iterator.inlines"
-#include "Propagator/Propagator.const_iterator.inlines"
+
+#include "Propagator/Propagator.view.inlines"
+// #include "Propagator/Propagator.const_view.inlines"
+
+// #include "Propagator/Propagator.iterator.inlines"
+// #include "Propagator/Propagator.const_iterator.inlines"
 
 #include "Propagator/Propagator_destroy.template"
 #include "Propagator/Propagator_isolate.template"

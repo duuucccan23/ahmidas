ca06eec62b8d1c98c40eced5839d845a9c25d37c
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2009-09-22 09:25:41 +0000

    Aggregate minor changes

--- src/L1/Source/Point.h
+++ src/L1/Source/Point.h
@@ -4,20 +4,50 @@
 
 namespace Source
 {
-  template< size_t L, size_t T >
+  template< size_t L, size_t T, Base::SourceType Type >
   class Point
+  {};
+
+  template< size_t L, size_t T >
+  class Point< L, T, (sou_SINGLET | sou_UNPOLARIZED) >
   {
-    size_t d_coord[3];
+    size_t d_coord[4];
 
     public:
-      Point(size_t const x, size_t const y, size_t const z);
+      Point(size_t const x, size_t const y, size_t const z, size_t const t );
+      Point(size_t const *coord);
+
+      size_t coord(Base::SpaceTimeIndex const idx) const;
+      size_t const *coord() const;
+  };
+
+  template< size_t L, size_t T >
+  class Point< L, T, (sou_SINGLET | sou_PARTLY_POLARIZED) >
+  {
+    size_t       d_coord[4];
+    std::complex d_field[4];
+
+    public:
+      Point(size_t const x, size_t const y, size_t const z, size_t const t );
+      Point(size_t const *coord);
+
+      size_t coord(Base::SpaceTimeIndex const idx) const;
+      size_t const *coord() const;
+  };
+
+  template< size_t L, size_t T >
+  class Point< L, T, (sou_SINGLET | sou_FULLY_POLARIZED) >
+  {
+    size_t           d_coord[4];
+    Base::DiracIndex d_index;
+
+    public:
+      Point(size_t const x, size_t const y, size_t const z, size_t const t );
       Point(size_t const *coord);
       size_t coord(Base::SpaceTimeIndex const idx) const;
       size_t const *coord() const;
   };
 }
-
-namespace Sink = Source;
 
 #include "Point/Point.inlines"
 #include "Point/Point_Point.template"

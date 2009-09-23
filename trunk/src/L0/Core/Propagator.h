b3900f9412afa520a2f5dc92fa1e170f81848677
dinter@ifh.de <dinter@ifh.de@34a3b8a4-774f-0410-9567-b5325461a675>
2009-09-23 11:08:40 +0000

    number of Dirac indices and Colour indices are no longer template arguments, but private members of Propagator

--- trunk/src/L0/Core/Propagator.h
+++ trunk/src/L0/Core/Propagator.h
@@ -8,9 +8,14 @@ namespace Core
 {
   // NOTE nDirac is the  n u m b e r  of Dirac indices, not a particualar Dirac index
   // same is true for nColour , being the number of colour indices
-  template< size_t L, size_t T, size_t nDirac, size_t nColour >
+  template< size_t L, size_t T >
   class Propagator
   {
+    size_t *nDirac;
+    size_t *nColour;
+
+    size_t psize;
+
     size_t *d_references;
 
     Core::Field< std::complex< double >, L, T > *const *d_components;
@@ -19,15 +24,16 @@ namespace Core
     size_t **dirac_strides;
 
     public:
-      Propagator();
-      Propagator(Propagator const &other);
-      Propagator(view< L, T, nDirac, nColour > const &view);
-      ~Propagator();
-
-#include "Propagator/Propagator.operators"
 
 #include "Propagator/Propagator.view"
 // #include "Propagator/Propagator.const_view"
+
+      Propagator(size_t nCol=2, size_t nDir=2);
+      Propagator(Propagator const &other);
+      Propagator(view< L, T > const &view);
+      ~Propagator();
+
+#include "Propagator/Propagator.operators"
 
 // #include "Propagator/Propagator.iterator"
 // #include "Propagator/Propagator.const_iterator"
@@ -41,29 +47,39 @@ namespace Core
 //       const_iterator end(Base::ColourIndex const idx, const size_t ColourID) const;// 
 //       const_iterator begin(Base::DiracIndex const idx, const size_t DiracID) const;
 //       const_iterator end(Base::DiracIndex const idx, const size_t DiracID) const;
-
-      view< L, T, nDirac-1, nColour > &operator()(Base::DiracIndex const idx, const size_t DiracID);
-      view< L, T, nDirac, nColour-1 > &operator()(Base::ColourIndex const idx, const size_t ColourID);
-      
-//       const_view< L, T, nDirac-1, nColour > const &operator()(Base::DiracIndex const idx, const size_t DiracID) const;      
-//       const_view< L, T, nDirac, nColour-1 > const &operator()(Base::ColourIndex const idx, const size_t ColourID) const;
       
 
-      size_t size() const;
+      size_t const size() const;
+      size_t const numDirac() const;
+      size_t const numColour() const;
+
+/* NOTE we should include something like Propagator*Gamma*Propagator
+   and maybe some herm. conj. derived class
+   type cast of a propagator with one dirac and one colour index to a Core::Field< SU3::Spinor >
+   would simplify I/O for standard ILDG format
+*/ 
 
     private:
       void destroy();
       void isolate();
+
   };
+
+  // typedef Propagator< L, T, 2, 2 > StandardPropagator;
+
 }
 
-#include "Propagator/Propagator.inlines"
+
 
 #include "Propagator/Propagator.view.inlines"
 // #include "Propagator/Propagator.const_view.inlines"
+
+#include "Propagator/Propagator.inlines"
 
 // #include "Propagator/Propagator.iterator.inlines"
 // #include "Propagator/Propagator.const_iterator.inlines"
 
 #include "Propagator/Propagator_destroy.template"
 #include "Propagator/Propagator_isolate.template"
+
+

513ccb961aab418f9599af4e0d4f2113577f2157
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2009-12-16 12:26:09 +0000

    Modified spatial and temporal plaquette definitions to match those of hmc code, a note is forthcoming.

--- trunk/example/confrw.cpp
+++ trunk/example/confrw.cpp
@@ -1,4 +1,4 @@
-// $Id: main.cpp 305 2009-12-14 12:56:19Z Albert.Deuzeman $
+// $Id: confrw.cpp 305 2009-12-14 12:56:19Z Albert.Deuzeman $
 #include <string>
 #include <iomanip>
 #include <iostream>
@@ -6,16 +6,28 @@
 #include <L0/Tool/IO.h>
 #include <L0/Core/Field.h>
 #include <L0/QCD/Gauge.h>
+#include <L1/Tool.h>
 
 int main(int argc, char **argv)
 {
   //This needs some cleaning up, but it is automatically updated by SVN
-  std::string Id = "$Id: main.cpp 305 2009-12-14 12:56:19Z Albert.Deuzeman $";
+  std::string Id = "$Id: confrw.cpp 305 2009-12-14 12:56:19Z Albert.Deuzeman $";
   std::cout << "Executable tag: " << Id.substr(1,Id.length()-2) << std::endl;
+
   std::cout << "Reading gauge file conf.88 from test directory.\n";
   Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../../test/conf.88");
+
+  double plaqs = Tool::spatialPlaquette(myfield);
+  std::cout << "Spatial plaquette value: " << plaqs << std::endl;
+
+  double plaqt = Tool::temporalPlaquette(myfield);
+  std::cout << "Temporal plaquette value: " << plaqt << std::endl;
+  std::cout << "Summmed plaquette value: " << 0.5 * (plaqt + plaqs) << std::endl;
+
+  std::cout << "\nThe summed plaquette value should match the one reported by viewing lime_contents on this configuration file.\n\n";
+  
   std::cout << "Writing to conf.copy.88 in test directory.\n";
   Tool::IO::saveILDG(myfield, "../../test/conf.copy.88");
-  std::cout << "To compare, extract the matching records from the two files with lime_extract_record, and diff.\n";
+  std::cout << "To compare the binary info, extract the matching records from the two files with lime_extract_record, and diff.\n";
   return 0;
 }

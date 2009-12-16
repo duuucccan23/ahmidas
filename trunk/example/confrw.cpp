18d939357aef0a9a0509c3846bb44e6aa40d5a8a
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2009-12-16 10:47:18 +0000

    Modified the main code to do a simple gauge read and write test, added example directory to include these kinds of codes.

--- trunk/example/confrw.cpp
+++ trunk/example/confrw.cpp
@@ -1,4 +1,4 @@
-// $Id$
+// $Id: main.cpp 305 2009-12-14 12:56:19Z Albert.Deuzeman $
 #include <string>
 #include <iomanip>
 #include <iostream>
@@ -10,12 +10,12 @@
 int main(int argc, char **argv)
 {
   //This needs some cleaning up, but it is automatically updated by SVN
-  std::string Id = "$Id$";
+  std::string Id = "$Id: main.cpp 305 2009-12-14 12:56:19Z Albert.Deuzeman $";
   std::cout << "Executable tag: " << Id.substr(1,Id.length()-2) << std::endl;
   std::cout << "Reading gauge file conf.88 from test directory.\n";
-  Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../test/conf.88");
+  Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../../test/conf.88");
   std::cout << "Writing to conf.copy.88 in test directory.\n";
-  Tool::IO::saveILDG(myfield, "../test/conf.copy.88");
+  Tool::IO::saveILDG(myfield, "../../test/conf.copy.88");
   std::cout << "To compare, extract the matching records from the two files with lime_extract_record, and diff.\n";
   return 0;
 }

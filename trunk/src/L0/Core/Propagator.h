85f3f5b7d013996b443085e4372bd65ba3d0b949
dinter@ifh.de <dinter@ifh.de@34a3b8a4-774f-0410-9567-b5325461a675>
2009-12-17 13:03:51 +0000

    some changes concerning Propagator class

--- trunk/src/L0/Core/Propagator.h
+++ trunk/src/L0/Core/Propagator.h
@@ -6,37 +6,41 @@
 
 namespace Core
 {
-  // NOTE nDirac is the  n u m b e r  of Dirac indices, not a particualar Dirac index
-  // same is true for nColour , being the number of colour indices
+  /* a propagator is a tensorial stucture with 2 Dirac and 2 Colour indices,
+     each for source and sink, respectively, for all lattice sites 
+     (being sink locations for a constant source)
+     maybe at some point we should include the source properties, in whatever format,
+     as a member variable
+  */
   template< size_t L, size_t T >
   class Propagator
   {
-    size_t *nDirac;
-    size_t *nColour;
+    static const size_t nDirac  = 2;
+    static const size_t nColour = 2;
 
     size_t psize;
 
     size_t *d_references;
 
-    Core::Field< std::complex< double >, L, T > *const *d_components;
+    Core::Field< std::complex< double >, L, T > **d_components;
 
-    size_t **colour_strides;
-    size_t **dirac_strides;
+    size_t *colour_strides;
+    size_t *dirac_strides;
 
     public:
 
-#include "Propagator/Propagator.view"
+// #include "Propagator/Propagator.view"
 // #include "Propagator/Propagator.const_view"
 
-      Propagator(size_t nCol=2, size_t nDir=2, bool alloc=true);
+      Propagator(bool alloc=true);
       Propagator(Propagator const &other);
-      Propagator(view< L, T > const &view); // this constructor makes a deep copy
+//       Propagator(view< L, T > const &view); // this constructor makes a deep copy
       ~Propagator();
 
 #include "Propagator/Propagator.operators"
 
 #include "Propagator/Propagator.iterator"
-#include "Propagator/Propagator.const_iterator"
+// #include "Propagator/Propagator.const_iterator"
 
       iterator<1> begin(Base::ColourIndex const idx, const size_t ColourID);
       iterator<1> end(Base::ColourIndex const idx, const size_t ColourID);
@@ -65,13 +69,11 @@ namespace Core
 
   };
 
-  // typedef Propagator< L, T, 2, 2 > StandardPropagator;
-
 }
 
 
 
-#include "Propagator/Propagator.view.inlines"
+// #include "Propagator/Propagator.view.inlines"
 // #include "Propagator/Propagator.const_view.inlines"
 
 #include "Propagator/Propagator.inlines"
--- trunk/src/simon_test.cpp
+++ trunk/src/simon_test.cpp
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <complex>
 #include <iomanip>
 #include <iostream>
 #include <fstream>
@@ -7,13 +8,19 @@
 
 #include <L0/Tool/IO.h>
 #include <L0/Core/Field.h>
+#include <L0/Core/Propagator.h>
 #include <L0/QCD/Gauge.h>
 // #include <L1/Smear/APE.h>
 
 int main(int argc, char **argv)
 {
 
-  std::cout << "this it a test ..." << std::endl;
+  const size_t L = 4;
+  const size_t T = 8;
+  
+  std::cout << "this is a test ..." << std::endl;
+
+  Core::Propagator<L, T>();
 
 //   const std::string gf_in = "/afs/ifh.de/group/etmc/scratch/poola/dinter/gauge_fields/16x16x16x32/conf.1500";
 //   const std::string gf_out =  "/afs/ifh.de/group/nic/scratch/poolb/dinter/tmp/conf.1500";
@@ -25,16 +32,16 @@ int main(int argc, char **argv)
 //   Tool::IO::saveILDG(myfield, gf_out);
 //   // check!
 
-  const size_t T = 8;
-  const size_t L = 4;
-  const std::string src_ildg = "./ahmidas/test/point_src.48";
-  Core::Field< QCD::Spinor, L, T > source_field = Tool::IO::loadILDG<QCD::Spinor, L, T >(src_ildg);
-
-  const std::string gf_ildg = "./ahmidas/test/conf.48";
-  Core::Field< QCD::GaugeSpinor, L, T > gauge_field = Tool::IO::loadILDG<QCD::Gauge, L, T >(gf_ildg);
-
-  std::cout << source_field[0];
-  std::cout << gauge_field[0];
+//   const size_t T = 8;
+//   const size_t L = 4;
+//   const std::string src_ildg = "./ahmidas/test/point_src.48";
+//   Core::Field< QCD::Spinor, L, T > source_field = Tool::IO::loadILDG<QCD::Spinor, L, T >(src_ildg);
+// 
+//   const std::string gf_ildg = "./ahmidas/test/conf.48";
+//   Core::Field< QCD::GaugeSpinor, L, T > gauge_field = Tool::IO::loadILDG<QCD::Gauge, L, T >(gf_ildg);
+// 
+//   std::cout << source_field[0];
+//   std::cout << gauge_field[0];
 
   return 0;
 }

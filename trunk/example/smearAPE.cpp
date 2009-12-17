318f13a63c16057033704e34a5c612bf498d9dc4
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2009-12-17 15:43:37 +0000

    Added another small example: how to APE smear a gaugefield. This was tested a while ago, but is hard to do exactly since the reunitarization is not exact between different codes.

---
+++ trunk/example/smearAPE.cpp
@@ -0,0 +1,35 @@
+// $Id$
+#include <string>
+#include <iomanip>
+#include <iostream>
+
+#include <L0/Tool/IO.h>
+#include <L0/Core/Field.h>
+#include <L0/QCD/Gauge.h>
+#include <L1/Smear/APE.h>
+
+int main(int argc, char **argv)
+{
+  //This needs some cleaning up, but it is automatically updated by SVN
+  std::string Id = "$Id$";
+  std::cout << "Executable tag: " << Id.substr(1,Id.length()-2) << std::endl;
+
+  std::cout << "Reading gauge file conf.88 from test directory.\n";
+  Core::Field< QCD::Gauge, 8, 8 > myfield = Tool::IO::loadILDG<QCD::Gauge, 8, 8 >("../../test/conf.88");
+
+  std::cout << "Showing the gauge link in the x-up direction from point (0,0,0,0).\n";
+  std::cout << myfield[0][Base::idx_X];
+
+  double strength = 0.5;
+  Smear::APE smearer = Smear::APE(strength); //set up a APE smearer with kappa=0.5
+
+  size_t times = 10;
+  std::cout << "APE smearing gauge field " << times << " times with strength " << strength << ".\n";
+
+  smearer.smear(myfield, times);
+
+  std::cout << "\nShowing the now smeared gauge link in the x-up direction from point (0,0,0,0).\n";
+  std::cout << myfield[0][Base::idx_X];
+
+  return 0;
+}

8c71e33324783ee21674318e65823a3e58e2fb4c
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2010-01-21 14:23:46 +0000

    Added some test data for contraction comparison.

--- test/loadMILC.cpp
+++ test/loadMILC.cpp
@@ -7,11 +7,12 @@
 
 int main(int argc, char **argv)
 {
-  Core::Field< QCD::Gauge > field = Tool::IO::loadMILC< QCD::Gauge >("lat.sample.16x6.milc", 16, 6);
+  Core::Field< QCD::Gauge > field = Tool::IO::loadMILC< QCD::Gauge >("../../test/lat.sample.16x6.milc", 16, 6);
   double sp = Tool::spatialPlaquette(field);
   double tp = Tool::temporalPlaquette(field);
   std::cout << "Spatial plaquette:  " << sp << ", should be 0.396411." << std::endl;
   std::cout << "Temporal plaquette: " << tp << ", should be 0.399299." << std::endl;
+
   if ((abs(sp - 0.396411) < 1E-6) && (abs(tp - 0.399299) < 1E-6))
     return 0;
   return 1;

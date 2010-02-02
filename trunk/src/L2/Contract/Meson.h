7476dfe701fc2f1d5a96428d4f75e097a50310d6
dinter@ifh.de <dinter@ifh.de@34a3b8a4-774f-0410-9567-b5325461a675>
2010-02-02 09:37:50 +0000

    Committed some intermediate state.

--- trunk/src/L2/Contract/Meson.h
+++ trunk/src/L2/Contract/Meson.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <string>
 
 #include <L0/Base/Base.h>
 #include <L0/Dirac/Gamma.h>
@@ -11,7 +12,7 @@
 
 namespace Contract
 {
-
+  
   template< size_t IndexSrc, size_t IndexSnk >
   Core::Correlator light_meson_twopoint(Core::Propagator const *u, Core::Propagator const *d,
                                         Dirac::Gamma< IndexSrc > const &interpolSrc,
@@ -22,6 +23,13 @@ namespace Contract
                                         Dirac::Gamma< IndexSrc > const &interpolSrc,
                                         Dirac::Gamma< IndexSnk > const &interpolSnk,
                                         size_t const *momentum);
+  
+  // this works using the one-end trick and gives all 16 gamma-combinations
+  // return value: Array of 16 Core::Correlator arranged as follows:
+  // gamma0, gamma1, gamma2, gamma3, unity, gamma5
+  // gamma5*gamma0, gamma5*gamma1, gamma5*gamma2, gamma5*gamma3,
+  // sigma01, sigma02, sigma03, sigma12, sigma13, sigma23
+  inline Core::Correlator *light_meson_twopoint_stochastic(Core::Propagator const *u);
 }
 
 #include "Meson/Meson.inlines"

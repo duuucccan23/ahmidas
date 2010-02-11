ad3bf7c2538f7062b18c2651b0a3c7e6655bdbb5
dinter <dinter@ifh.de>
2010-02-11 19:14:43 +0000

    Intermediate commit, parallel code still does not run properly which can be traced back to the Propagator::destroy() routine calling the Field destructor.

--- arch/mpi/L0/Base/Weave/Weave_sumOverTimeSlices_a.cpp
+++ arch/mpi/L0/Base/Weave/Weave_sumOverTimeSlices_a.cpp
@@ -4,6 +4,7 @@ namespace Base
 {
   void Weave::sumOverTimeSlices(std::complex< double > const *data_send, std::complex< double > *data_recv, size_t const count) const
   {
+    d_grid.grid().Barrier();
     // call MPI::Intracomm::Allreduce(...)
     (d_grid.grid()).Allreduce(static_cast< const void * >(data_send), static_cast< void * >(data_recv),
                            int(count*d_T), MPI_DOUBLE_COMPLEX, MPI_SUM);

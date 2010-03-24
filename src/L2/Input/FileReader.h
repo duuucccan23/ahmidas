abe25bebf00083bd0e44e70abe16d5592deb93d7
dinter <dinter@ifh.de>
2010-03-24 18:05:36 +0000

    Intermediate commit, won't compile

--- src/L2/Input/FileReader.h
+++ src/L2/Input/FileReader.h
@@ -22,47 +22,79 @@
 namespace Input
 {
 
+  enum FileTag
+  {
+    tag_NOFILE,
+    tag_FILE
+  };
+
+  class FileReader;
+
+  class File
+  {
+    friend class FileReader;
+
+    std::string d_type;
+    std::string d_directory;
+    std::string d_filenameBase;
+    std::string d_filenameEnding;
+    int d_firstIndex;
+    int d_lastIndex;
+    size_t d_indexWidth;
+    
+    public:
+    
+      File(std::string const type, std::string const directory,
+           std::string const filenameBase, std::string const filenameEnding,
+           std::string const firstIndex, std::string const lastIndex, std::string const indexWidth);
+      
+  };
+
   class FileReader
   {
+  
 
-    enum ValType
-    {
-      type_FLOAT = 1,
-      type_INTEGER = 2,
-      type_SIZE_T = 3,
-      type_STRING = 4
-    };
 
-    enum InputTag
-    {
-      type_FILES = 100,
-      type_PARAM = 200
-    };
+//     enum ValType
+//     {
+//       type_FLOAT = 1,
+//       type_INTEGER = 2,
+//       type_SIZE_T = 3,
+//       type_STRING = 4
+//     };
+// 
+//     enum InputTag
+//     {
+//       type_FILES = 100,
+//       type_PARAM = 200
+//     };
 
     static const size_t buf_size = 1000;
 
 
-//     std::multimap< int, size_t >      mm_size_ts;
-//     std::multimap< int, int >         mm_integers;
-//     std::multimap< int, double >      mm_floats;
-//     std::multimap< int, std::string > mm_strings;
+    std::stack< std::string > open_tags;
 
-    std::multimap< InputTag, std::pair< std::string, std::string > > input;
-
-    std::vector< std::vector< std::string > > files;
-
+    std::multimap< std::string, std::string > input;
+    
+    // this one does not store the filenames but the raw data
+    std::vector< File > files;
+    
+    void line_error(size_t const line, std::string const message = "");
+    
 
     public:
 
     FileReader(std::string file);
-
-    size_t getL();
-    size_t getT();
-
-    std::vector< std::vector< std::string > > const &getFiles();
-
-    size_t * getPosition(int tag);
+    
+    void initializeParameters(size_t &L, size_t &T, 
+                         std::vector< std::vector< std::string > > &filenames, 
+                         std::vector< double > &floats, 
+                         std::vector< size_t * > &positions, 
+                         std::vector< int > &operators) const;
 
   };
 }
+
+#include "FileReader/FileReader.inlines"
+
 #endif
--- src/L2/Input/FileReader/FileReader.cpp
+++ src/L2/Input/FileReader/FileReader.cpp
@@ -26,13 +26,67 @@ namespace Input
     line.reserve(buf_size);
 
     char * buffer = new char[buf_size];
+    int state(0), global_state(0);
+    size_t line_counter(0);
 
-    while(fin.getline(buffer, buf_size))
+    while (fin.getline(buffer, buf_size))
     {
+      line_counter++;
       line = buffer;
-      parser.parseLine(line, ID, contents);
+      state = parser.parseLine(line, ID, contents);
+      global_state += state;
+      switch (state)
+      { 
+        case tag_OPENING:
+          open_tags.push(ID);
+          break;
+
+        case tag_CLOSING:
+          if(open_tags.top() == ID)
+          {
+            open_tags.pop();
+          }
+          else
+          {
+            line_error(line_counter, "Unmatched opening tag!");
+          }
+          break;
+
+        case tag_COMPLETE:
+          input.insert(std::make_pair(ID, contents));
+          break;
+
+        default:
+          line_error(line_counter, "Unmatched opening tag!");
+      }
+      
+    } // while (fin.getline(buffer, buf_size))
+    
+    if (global_state != 0)
+    {
+      std::cerr << "Unknown error occurred after file " << file << " had been read."  << std::endl;
+      exit(100);
     }
+    
     delete [] buffer;
     fin.close();
   }
+  
+  
+  // there should be an error handling here
+  void FileReader::initializeParameters(size_t &L, size_t &T, 
+                                        std::vector< std::vector< std::string > > &filenames, 
+                                        std::vector< double > &floats, 
+                                        std::vector< size_t * > &positions, 
+                                        std::vector< int > &operators) const
+  {
+    std::multimap< std::string, std::string >::iterator It; 
+    for (It=input.begin(); It!=input.end(); It++)
+    {
+         
+    }
+
+    
+  }                                   
+
 }

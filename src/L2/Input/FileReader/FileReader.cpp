cb9b7e43a1722edc56c0749a4eb725be1e4e24fc
dinter <dinter@ifh.de>
2010-03-24 22:50:36 +0000

    Further implementation of FileReader. Some test to be following soon.

--- src/L2/Input/FileReader/FileReader.cpp
+++ src/L2/Input/FileReader/FileReader.cpp
@@ -28,22 +28,39 @@ namespace Input
     char * buffer = new char[buf_size];
     int state(0), global_state(0);
     size_t line_counter(0);
+    
+    bool fileMode = false;
+    std::string * fileData = NULL;
 
     while (fin.getline(buffer, buf_size))
     {
       line_counter++;
       line = buffer;
       state = parser.parseLine(line, ID, contents);
-      global_state += state;
       switch (state)
-      { 
+      {
         case tag_OPENING:
+          global_state += state;
+          if (ID == "file")
+          {
+            fileMode = true;
+            fileData = new std::string[7];
+          }
           open_tags.push(ID);
           break;
 
         case tag_CLOSING:
+          global_state += state;
           if(open_tags.top() == ID)
           {
+            if (ID == "file")
+            {
+              files.push_back(File(fileData[0], fileData[1], fileData[2], fileData[3],
+                                   fileData[4], fileData[5], fileData[6]));
+              fileMode = false;
+              delete [] fileData;
+              fileData = NULL;
+            }
             open_tags.pop();
           }
           else
@@ -53,12 +70,52 @@ namespace Input
           break;
 
         case tag_COMPLETE:
-          input.insert(std::make_pair(ID, contents));
+          if (!fileMode)
+          {
+            input.insert(std::make_pair(ID, contents));
+          }
+          else
+          {
+            if (ID == "type")
+            {
+              fileData[0] = contents;
+            }
+            else if (ID == "directory")
+            {
+              fileData[1] = contents;
+            }
+            else if (ID == "filenameBase")
+            {
+              fileData[2] = contents;
+            }
+            else if (ID == "filenameEnding")
+            {
+              fileData[3] = contents;
+            }
+            else if (ID == "firstIndex")
+            {
+              fileData[4] = contents;
+            }
+            else if (ID == "lastIndex")
+            {
+              fileData[5] = contents;
+            }
+            else if (ID == "indexWidth")
+            {
+              fileData[6] = contents;
+            }
+          }
+          break;
+
+        case tag_COMMENT:
+          break;
+        case tag_HEADLINE:
           break;
 
         default:
+          std::cerr << state << std::endl;
           line_error(line_counter, "Unmatched opening tag!");
-      }
+      }// switch (state)
       
     } // while (fin.getline(buffer, buf_size))
     
@@ -76,17 +133,70 @@ namespace Input
   // there should be an error handling here
   void FileReader::initializeParameters(size_t &L, size_t &T, 
                                         std::vector< std::vector< std::string > > &filenames, 
-                                        std::vector< double > &floats, 
+                                        std::map< std::string, double > &floats, 
                                         std::vector< size_t * > &positions, 
-                                        std::vector< int > &operators) const
+                                        std::map< std::string, int > &operators) const
   {
-    std::multimap< std::string, std::string >::iterator It; 
+    for (size_t iF=0; iF<filenames.size(); iF++)
+      filenames[iF].clear();
+    filenames.clear();
+    floats.clear();
+    positions.clear();
+    operators.clear();
+    std::map< std::string, std::string >::const_iterator It; 
     for (It=input.begin(); It!=input.end(); It++)
     {
-         
+      if((*It).first == "L")
+      {
+        L = atoi((*It).second.c_str());
+        std::cout << "L = " << L << std::endl;
+        continue;
+      }
+      if((*It).first == "T")
+      {
+        T = atoi((*It).second.c_str());
+        std::cout << "T = " << T << std::endl;
+        continue;
+      }
+      if((*It).first == "position")
+      {
+        size_t *pos = new size_t[4];
+        // have to split the string here ... 
+        pos[0] = atoi((*It).second.c_str());
+        // this has to be implemented 
+        std::cerr << "This is not implemented in FileReader::initializeParameters(...)" << std::endl;
+        pos[1] = pos[2] = pos[3] = -1;
+        positions.push_back(pos);
+        continue;
+      }
+      if((*It).first == "kappa")
+      {
+        floats.insert(make_pair((*It).first, atof((*It).second.c_str())));
+        continue;
+      }
+      if((*It).first == "mu")
+      {
+        floats.insert(make_pair((*It).first, atof((*It).second.c_str())));
+        continue;
+      }
     }
-
-    
+    for (size_t iF=0; iF<files.size(); iF++)
+    {
+      std::vector< std::string > tmp;
+      //NOTE: some checks are due here!!!
+      for (size_t idx=(files[iF]).d_firstIndex; idx<=(files[iF]).d_lastIndex; idx++)
+      {
+        std::ostringstream oss;
+        oss  <<  (files[iF]).d_directory << "/" <<  (files[iF]).d_filenameBase;
+        oss.fill('0');
+        oss.width((files[iF]).d_indexWidth);
+        oss << idx;
+        oss <<  (files[iF]).d_filenameEnding;
+        oss.flush();
+        tmp.push_back(oss.str());
+      }
+      filenames.push_back(tmp);
+    }
   }                                   
 
 }

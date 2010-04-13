29fc1cb1204ab32bd77d3e863b93d757689daae9
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2010-04-13 11:04:03 +0000

    Fixed the test, further rewriting of Propagator_changeBoundaryConditions.cpp

--- trunk/src/L0/Core/Propagator/Propagator_changeBoundaryConditions.cpp
+++ trunk/src/L0/Core/Propagator/Propagator_changeBoundaryConditions.cpp
@@ -2,33 +2,30 @@
 
 namespace Core
 {
-
   void Propagator::changeBoundaryConditions_uniformToFixed(size_t timesliceSource, size_t timesliceBoundary)
   {
-    size_t T(d_components->T());
-    size_t L(d_components->L());
-
-    assert(timesliceSource < T);
-    assert(timesliceBoundary < T);
+    Base::Weave weave(L(), T());
+    size_t localIndex;
+    std::complex< double > phase;
+    long idx_T_new;
 
     std::cout << "Changing boundary conditions of propagator: uniform to fixed.\n";
-
-    Base::Weave weave(L, T);
+    assert(timesliceSource < T());
+    assert(timesliceBoundary < T());
     isolate();
-    size_t idx_T, idx_X, idx_Y, idx_Z, localIndex;
 
-    long const t_s = timesliceSource > (timesliceBoundary ? timesliceSource : timesliceSource - T);
-    std::complex< double > const const_phase(exp(std::complex< double >(0, - M_PI * double(t_s) / double(T))));
+    long const t_s = timesliceSource - (timesliceSource > timesliceBoundary ? 0 : T());
+    std::complex< double > const const_phase(exp(std::complex< double >(0, - M_PI * t_s / T())));
 
-    for(idx_T=0; idx_T<T; idx_T++)
+    for(size_t idx_T = 0; idx_T < T(); idx_T++)
     {
-      long const idx_T_new = idx_T > (timesliceBoundary ? idx_T : idx_T - T);
-      std::complex< double > const phase(const_phase*exp(std::complex< double >(0, M_PI*double(idx_T_new)/double(T))));
-      for(idx_Z=0; idx_Z < L; idx_Z++)
+      idx_T_new = idx_T - (idx_T > timesliceBoundary ? 0 : T());
+      phase = const_phase * exp(std::complex< double >(0, M_PI * idx_T_new / T()));
+      for(size_t idx_Z = 0; idx_Z < L(); idx_Z++)
       {
-        for(idx_Y=0; idx_Y < L; idx_Y++)
+        for(size_t idx_Y = 0; idx_Y < L(); idx_Y++)
         {
-          for(idx_X=0; idx_X < L; idx_X++)
+          for(size_t idx_X = 0; idx_X < L(); idx_X++)
           {
             localIndex = weave.globalCoordToLocalIndex(idx_X, idx_Y, idx_Z, idx_T);
             /* globalCoordToLocalIndex returns local volume if local data is not available on this cpu */

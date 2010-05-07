ce5b6f0ccc6d5c62a9115e7adec58967707685cb
dinter@ifh.de <dinter@ifh.de@34a3b8a4-774f-0410-9567-b5325461a675>
2010-05-07 12:38:28 +0000

    Added smearing of sequential sources. As a reminder, I also created an empty function for applying the (twisted mass) Dirac operator to a Propagator. Modified error output of the loadScidac function, which now prints the name of the requested file.

---
+++ trunk/src/L2/Contract/Baryon/Baryon_create_sequential_source_proton_d_c.cpp
@@ -0,0 +1,67 @@
+#include "Baryon.ih"
+
+namespace Contract
+{
+  // this would do the trace over projector * sequential_source
+  void create_sequential_source_proton_d(Core:: Propagator &seqSrc,
+                                    Core::Propagator const &u1, Core::Propagator const &u2,
+                                    Core::Field < QCD::Gauge > &gauge_field,
+                                    Smear::fermionFieldSmearing const smearing,
+                                    // smearing: iterations and parameter
+                                    size_t const nSmear, double const pSmear,
+                                    size_t const t_snk, Base::BaryonPropagatorProjector const proj)
+  {
+
+    size_t const L(u1.L());
+    size_t const T(u1.T());
+    assert(L == u2.L()     && T == u2.T());
+    assert(L == seqSrc.L() && T == seqSrc.T());
+
+    //initialize with zero
+    seqSrc *= 0.0;
+
+    Base::Weave weave(L, T);
+
+    Dirac::Gamma< 5 > gamma5;
+    QCD::Tensor tmp;
+
+    size_t localIndex;
+    for(size_t idx_Z = 0; idx_Z < L; idx_Z++)
+    {
+      for(size_t idx_Y = 0; idx_Y < L; idx_Y++)
+      {
+        for(size_t idx_X = 0; idx_X < L; idx_X++)
+        {
+          localIndex = weave.globalCoordToLocalIndex(idx_X, idx_Y, idx_Z, t_snk);
+          /* globalCoordToLocalIndex returns local volume if local data is not available on this cpu */
+          if (localIndex == weave.localVolume())
+            continue;
+
+          QCD::make_sequential_d(tmp, u1[localIndex], u2[localIndex], proj);
+          tmp.conjugate();
+          // actually we have to multiply by the appropriate gamma combination "dagger"-ed,
+          // but in case of i*gamma2*gamma0*gamma5 the dagger does not change anything
+          tmp.right_multiply_proton();
+          // we absorb the other interpolating field Gamma structure into the source definition
+          // for conventional reasons: the contraction then looks the same for the u and d current
+          tmp.left_multiply_proton();
+
+          seqSrc[localIndex] = tmp;
+        }
+      }
+    }
+
+    switch (smearing)
+    {
+      case Smear::sm_Jacobi:
+        seqSrc.smearJacobi(pSmear, nSmear, gauge_field);
+        break;
+     default:
+       std::cerr << "unknown smearing in function defined in create_sequential_source_proton_d_c.cpp" << std::endl;
+       exit(1);
+    }
+
+    seqSrc.rightMultiply(gamma5);
+  }
+
+}

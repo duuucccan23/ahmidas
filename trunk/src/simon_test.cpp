ff209d8dd13a0f3bafa2ba7167b55ec895966f06
dinter@ifh.de <dinter@ifh.de@34a3b8a4-774f-0410-9567-b5325461a675>
2010-09-01 08:57:06 +0000

    Changed an executable in order to perform a scaling test.

--- trunk/src/simon_test.cpp
+++ trunk/src/simon_test.cpp
@@ -1,211 +1,202 @@
 
-#include <cstring>
-#include <vector>
-#include <map>
 #include <complex>
+#include <cstring>
+#include <map>
+#include <vector>
 #include <iomanip>
 #include <iostream>
-#include <fstream>
-#include <sstream>
 
-#include <L0/Base/Weave.h>
 #include <L0/Dirac/Gamma.h>
-#include <L0/Dirac/Matrix.h>
-#include <L0/QCD/Gauge.h>
+#include <L0/Core/Field.h>
 #include <L0/Core/Propagator.h>
-#include <L2/Contract/Baryon.h>
+#include <L0/Core/Propagator.h>
 #include <L1/Tool/IO.h>
+#include <L2/Input/FileReader.h>
 #include <L1/Smear.h>
 #include <L1/Smear/APE.h>
 #include <L1/Smear/Jacobi.h>
-#include <L2/Input/FileReader.h>
 
-#define __COMPENSATE_UNIFORM_BOUNDARY_CONDITIONS_FW__
-#define __COMPENSATE_UNIFORM_BOUNDARY_CONDITIONS_BW__
 
 int main(int argc, char **argv)
 {
 
-  size_t L_tmp = 0;
-  size_t T_tmp = 0;
 
-  Input::FileReader reader("../test/my_test_input_fixed_timeslice.xml");
+  // lattice size, to be read from input file (and therefore initialized to zero)
+  size_t L_tmp(0);
+  size_t T_tmp(0);
 
+
+  /* those are the containers to be filled by the input file reader */
+
+  // this one contains floating point variables like kappa, mu, ...
   std::map< std::string, double > floats;
-  std::vector< size_t * > positions;
-  std::map< std::string, int > operators;
+  // this one contains the file names (themselves stored in containers) of propagators (or sources, gauge fields, ...)
   std::vector< std::vector< std::string > > files;
 
+  std::vector< size_t * > positions;
+  std::map< std::string, int > operators;
+
+  /* ****************************************** */
+  /* ****** reading the input file ************ */
+  /* ****************************************** */
+
+  // create input file reader, the name of the input file has to be passed as a parameter
+  Input::FileReader reader("./generate_point_source_input.xml");
+
+  // get input parameters
+  // note: this is how to invoke a member function of an object in C++:
+  // <name of object>.<name of function>(<parameter list>)
   reader.initializeParameters(L_tmp, T_tmp, files, floats, positions, operators);
 
-  size_t const L(L_tmp);
-  size_t const T(T_tmp);
+  const size_t L(L_tmp);
+  const size_t T(T_tmp);
 
+  /* ****************************************** */
+
+  // this is needed if we want to have the output (i.e. to the standard output) done by only
+  // one process in the parallel version
   Base::Weave weave(L, T);
-  weave.barrier();
 
-  std::cout << "Lattice size: " << L << "x" << L << "x" << L << "x" << T << std::endl;
+  // that's how writing to the standard output works in C++
+  // note: the "<<" operator also works for most of the ahmidas objects like SU3::Spinor or QCD::Tensor
+  if(weave.isRoot())
+    std::cout << "\nLattice size: " << L << "x" << L << "x" << L << "x" << T << std::endl;
 
+  // that's how one can access the values in the map "floats"
   double kappa = floats["kappa"];
   double mu    = floats["mu"];
-  if (weave.isRoot())
+
+  if(weave.isRoot())
     std::cout << "kappa = " << kappa << ", mu = " << mu << std::endl;
+
+  size_t const * const source_position = positions[0];
+
+  if(weave.isRoot())
+    std::cout << "\nsource position: " << source_position[0] << " " << source_position[1] << " "
+                                       << source_position[2] << " " << source_position[3] << std::endl;
+
+
 
   double const APE_alpha      = floats["APE_param"];
   size_t const APE_iterations = size_t(floats["APE_steps"]);
-
   double const Jac_alpha      = floats["Jac_param"];
   size_t const Jac_iterations = size_t(floats["Jac_steps"]);
+
   if (weave.isRoot())
   {
     std::cout << "APE    smearing: parameter = " << APE_alpha << ", iterations = " << APE_iterations << std::endl;
     std::cout << "Jacobi smearing: parameter = " << Jac_alpha << ", iterations = " << Jac_iterations << std::endl;
   }
 
-  size_t const timeslice_source = (positions[0])[Base::idx_T];
-  if (weave.isRoot())
-    std::cout << "timeslice (source) = " << timeslice_source << std::endl;
-  // size_t const source_position[4] = {0,0,0,timeslice_source};
-  size_t const timeslice_boundary(T-1);
-  size_t const t_op = size_t(floats["t_insertion"]);
-  if (weave.isRoot())
-    std::cout << "timeslice (operator insertion) = " << t_op << std::endl;
+  // for simpler use let us reference the file names via convenient names
+  std::vector< std::string > const & pointSourceFiles(files[0]);
+  std::vector< std::string > const & gaugeFieldFiles(files[1]);
 
-  std::vector< std::string > const &propfilesD(files[0]);
-  std::vector< std::string > const &propfilesU(files[1]);
-  std::vector< std::string > const &gaugeFieldFiles(files[2]);
-  std::vector< std::string > const &sequentialSourceFilesD(files[3]);
-  std::vector< std::string > const &sequentialSourceFilesU(files[4]);
-  std::vector< std::string > const &sequentialPropFilesD(files[5]);
-  std::vector< std::string > const &sequentialPropFilesU(files[6]);
-
-  Core::Field< QCD::Gauge > gauge_field(L, T);
-
-  if (weave.isRoot())
-    std::cout << "gauge field to be read from " << gaugeFieldFiles[0] << " ... ";
-  Tool::IO::load(&gauge_field, gaugeFieldFiles[0], Tool::IO::fileILDG);
-  if (weave.isRoot())
-    std::cout << "done.\n" << std::endl;
+  double const TIME_FACTOR(1.0/CLOCKS_PER_SEC);
+  double time_read;
+  double time_smear;
+  double time_write;
 
 
-  if (APE_iterations > 0)
-  {
-    Smear::APE APE_tool(APE_alpha);
-    APE_tool.smear(gauge_field, APE_iterations);
-  }
+  Core::Field< QCD::Gauge > *gauge_field;
 
-  Smear::Jacobi Jacobi_tool(Jac_alpha);
-
-
-  Core::Propagator uProp(L, T);
-  Tool::IO::load(&uProp, propfilesU, Tool::IO::fileSCIDAC, 64);
-
-  if (weave.isRoot())
-    std::cout << "u quark propagator successfully loaded\n" << std::endl;
-
-  Core::Propagator dProp(L, T);
-  Tool::IO::load(&dProp, propfilesD, Tool::IO::fileSCIDAC, 64);
-
-  if (weave.isRoot())
-    std::cout << "d quark propagator successfully loaded\n" << std::endl;
-
-#ifdef __COMPENSATE_UNIFORM_BOUNDARY_CONDITIONS_FW__
-  dProp.changeBoundaryConditions_uniformToFixed(timeslice_source, timeslice_boundary);
-  uProp.changeBoundaryConditions_uniformToFixed(timeslice_source, timeslice_boundary);
-#endif
-
-  Core::Correlator p2p = Contract::proton_twopoint(uProp, uProp, dProp, Base::proj_NO_PROJECTOR);
-
-  if (weave.isRoot())
-  {
-    std::cout << "\nproton twopoint\n" << std::endl;
-    p2p *= Base::proj_PARITY_PLUS_TM;
-    std::cout << p2p << std::endl;
-  }
-  weave.barrier();
-
-  {
-
-  Core::Propagator sequentialSource_u(L, T);
-  Core::Propagator sequentialSource_d(L, T);
-
-
-  Contract::create_sequential_source_proton_fixed_insertion_timeslice(&sequentialSource_u, &sequentialSource_d,
-                                                            uProp, dProp, t_op, Base::op_GAMMA_4);
-
-
-  Tool::IO::save(&sequentialSource_d, sequentialSourceFilesD, Tool::IO::fileSCIDAC);
-  Tool::IO::save(&sequentialSource_u, sequentialSourceFilesU, Tool::IO::fileSCIDAC);
-
-  }
-
-  if (weave.isRoot())
-  {
-
-    //std::string const inversion_command_d ("../test/invert -f ../test/simon/invert_input_d >/dev/null");
-    //std::string const inversion_command_u ("../test/invert -f ../test/simon/invert_input_u >/dev/null");
-    std::string const inversion_command_d ("../test/invert -f ../test/simon/invert_input_d");
-    std::string const inversion_command_u ("../test/invert -f ../test/simon/invert_input_u");
-
-    int state = system(inversion_command_d.c_str());
-
-    if(state == 0)
-      std::cout << "sequential source (d) inverted successfully!" << std::endl;
-    else
-    {
-      std::cerr << "error occurred while trying to invert sequential source (d)!" << std::endl;
-      exit(1);
-    }
-
-    state = system(inversion_command_u.c_str());
-
-    if(state == 0)
-      std::cout << "sequential source (u) inverted successfully!" << std::endl;
-    else
-    {
-      std::cerr << "error occurred while trying to invert sequential source (u)!" << std::endl;
-      exit(1);
-    }
-  }
-
-  weave.barrier();
-
-  Core::Propagator sequentialPropagator_u(L, T);
-  Core::Propagator sequentialPropagator_d(L, T);
-
-  Tool::IO::load(&sequentialPropagator_d, sequentialPropFilesD, Tool::IO::fileSCIDAC);
-  Tool::IO::load(&sequentialPropagator_u, sequentialPropFilesU, Tool::IO::fileSCIDAC);
-
-#ifdef __COMPENSATE_UNIFORM_BOUNDARY_CONDITIONS_FW__
-  sequentialPropagator_u.changeBoundaryConditions_uniformToFixed(t_op, timeslice_boundary);
-  sequentialPropagator_d.changeBoundaryConditions_uniformToFixed(t_op, timeslice_boundary);
-#endif
 
 
   if (Jac_iterations > 0)
   {
-    uProp.smearJacobi(Jac_alpha, Jac_iterations, gauge_field);
-    dProp.smearJacobi(Jac_alpha, Jac_iterations, gauge_field);
-    sequentialPropagator_u.smearJacobi(Jac_alpha, Jac_iterations, gauge_field);
-    sequentialPropagator_d.smearJacobi(Jac_alpha, Jac_iterations, gauge_field);
+
+    gauge_field = new Core::Field< QCD::Gauge > (L, T);
+    if (weave.isRoot())
+      std::cout << "gauge field to be read from " << gaugeFieldFiles[0] << " ... " << std::flush;
+
+    weave.barrier();
+    double const time_1a = double(clock());
+
+    Tool::IO::load(gauge_field, gaugeFieldFiles[0], Tool::IO::fileILDG);
+
+    weave.barrier();
+    double const time_1b = double(clock());
+    time_read = (time_1b - time_1a) * TIME_FACTOR;
+
+    if (weave.isRoot())
+    std::cout << "done in "
+              << std::scientific << time_read
+              << " seconds.\n" << std::endl;
+
+
+    Smear::APE APE_tool(APE_alpha);
+
+    weave.barrier();
+    double const time_2a(clock());
+
+    // smear the gauge field
+    APE_tool.smear(*gauge_field, APE_iterations);
+
+    weave.barrier();
+    double const time_2b = double(clock());
+
+    time_smear = (time_2b - time_2a) * TIME_FACTOR;
+
+    if (weave.isRoot())
+      std::cout << "gauge field smeared successfully in "
+                << std::scientific << time_smear
+                << " seconds\n" << std::endl;
   }
 
-  Core::Correlator C3p_d  = Contract::proton_twopoint(uProp, uProp, sequentialPropagator_d, Base::proj_PARITY_PLUS_TM);
-  Core::Correlator C3p_u1 = Contract::proton_twopoint(uProp, sequentialPropagator_u, dProp, Base::proj_PARITY_PLUS_TM);
-  Core::Correlator C3p_u2 = Contract::proton_twopoint(sequentialPropagator_u, uProp, dProp, Base::proj_PARITY_PLUS_TM);
+  Core::Propagator point_source(L, T);
+  point_source *= 0.0;
 
-  C3p_u1 += C3p_u2;
+  size_t localIndex = weave.globalCoordToLocalIndex(source_position[Base::idx_X],
+                                                    source_position[Base::idx_Y],
+                                                    source_position[Base::idx_Z],
+                                                    source_position[Base::idx_T]);
+  if (localIndex != weave.localVolume())
+  {
+    point_source[localIndex] = QCD::Tensor::identity();
+  }
+  if (Jac_iterations > 0)
+  {
+    point_source.smearJacobi(Jac_alpha, Jac_iterations, *gauge_field);
+    delete gauge_field;
+    if(weave.isRoot())
+      std::cout << "point source smeared successfully\n" << std::endl;
+  }
+  // to prevent NANs in optimized code on timeslices where everything should be zero
+  point_source.select_timeslice(source_position[Base::idx_T]);
+  double norm(point_source.norm());
+  if (weave.isRoot())
+  {
+    std::cout.precision(10);
+    std::cout << std::scientific << "norm of point source: " << norm << std::endl << std::endl;
+    if(norm != norm) // happens if norm is 'nan'
+      exit(1);
+  }
+
+
+  weave.barrier();
+  double const time_3a = double(clock());
+
+
+  Tool::IO::save(&point_source, pointSourceFiles, Tool::IO::fileSCIDAC);
+
+  weave.barrier();
+  double const time_3b = double(clock());
+
+  time_write = (time_3b - time_3a) * TIME_FACTOR;
+
+  if (weave.isRoot())
+    std::cout << "point source saved successfully in "
+              << std::scientific << time_write
+              << " seconds\n" << std::endl;
 
   if (weave.isRoot())
   {
-    std::cout << "\n ubar gamma_0 u \n" << std::endl;
-    std::cout << C3p_u1 << std::endl;
-    std::cout << "\n dbar gamma_0 d \n" << std::endl;
-    std::cout << C3p_d << std::endl;
+    std::cerr.precision(2);
+    std::cerr << std::scientific << time_read  << "  "
+              << std::scientific << time_smear << "  "
+              << std::scientific << time_write << std::endl;
   }
 
-  weave.barrier();
-  std::cout << "\nprogramm is going to exit normally now\n" << std::endl;
-
+  // leave main function
   return EXIT_SUCCESS;
 }

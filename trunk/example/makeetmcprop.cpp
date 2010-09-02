c1f59077a618860578af25d20af590681c60d7f7
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2010-09-02 09:02:48 +0000

    Adding more checks for the Scidac propagators, and adding a tool to add the required etmc headers to really old propagators.

--- trunk/example/makeetmcprop.cpp
+++ trunk/example/makeetmcprop.cpp
@@ -5,17 +5,18 @@
 
 int main(int argc, char **argv)
 {
-  if (argc != 4)
+  if (argc != 5)
   {
     std::cerr << "This program expects the following input:\n";
-    std::cerr << argv[0] << " [L/a] [T/a] [filename]\n";
-    std::cerr << "And will then load the file [filename] and safe it with etmc propagator headers.\n";
+    std::cerr << argv[0] << " [L/a] [T/a] [filename] [32|64]\n";
+    std::cerr << "And will then load the file [filename] and save it with ETMC propagator Lime information.\n";
     exit(EXIT_FAILURE);
   }
   size_t L = atoi(argv[1]);
   size_t T = atoi(argv[2]);
+  size_t prec = atoi(argv[4]);
   Core::Field< QCD::Spinor > myprop(L,T);
-  Tool::IO::loadScidacUnsafe(&myprop, argv[3], 32);
+  Tool::IO::loadScidacUnsafe(&myprop, argv[3], prec);
   Tool::IO::saveScidac(myprop, argv[3]);
   return 0;
 }

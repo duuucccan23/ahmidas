35186686f48865377ef0f2d5cc9f50a40c537517
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2010-09-02 16:07:46 +0000

    Scidac checksum now works for both gauge and spinor fields. Some minor error reporting fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 #include <L0/Core/Field.h>
-#include <L0/QCD/Spinor.h>
+#include <L0/QCD/Gauge.h>
 #include <L1/Tool/IO.h>
+#include <L1/Tool/ScidacChecksum.h>
 
 int main(int argc, char **argv)
 {
-  if (argc != 4)
-  {
-    std::cerr << "This program expects the following input:\n";
-    std::cerr << argv[0] << " [L/a] [T/a] [filename]\n";
-    std::cerr << "And will then load the file [filename] and safe it with etmc propagator headers.\n";
-    exit(EXIT_FAILURE);
-  }
-  size_t L = atoi(argv[1]);
-  size_t T = atoi(argv[2]);
-  Core::Field< QCD::Spinor > myprop(L,T);
-  Tool::IO::loadScidacUnsafe(&myprop, argv[3], 32);
-  Tool::IO::saveScidac(myprop, argv[3]);
+  Core::Field< QCD::Gauge > mygauge(4,8);
+  Tool::IO::loadILDG(&mygauge, "../test/conf.48");
   return 0;
 }

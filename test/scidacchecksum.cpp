fa2f82c997984a80ca47de09e69b5f9b3eb597f4
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2010-09-03 13:40:23 +0000

    Added a simplified Scidac checksum test. Once the parsing of the checksum Lime message is included in the main code, this should become a more full test. Now user input is needed.

--- test/scidacchecksum.cpp
+++ test/scidacchecksum.cpp
@@ -7,11 +7,14 @@
 int main(int argc, char **argv)
 {
   Core::Field< QCD::Gauge > mygauge(4,8);
-  Tool::IO::loadILDG(&mygauge, "../test/conf.48");
+  Tool::IO::loadILDG(&mygauge, "../../test/conf.48");
   std::cout << "In headers: 0x1c231a18 0x3c553b.\n\n";
 
   Core::Field< QCD::Spinor > myspin(4,4);
-  Tool::IO::loadScidac(&myspin, "../test/source4x4_d.00.inverted");
+  Tool::IO::loadScidac(&myspin, "../../test/source4x4_d.00.inverted");
   std::cout << "In headers: 0xb3c083ec 0xd4211a35.\n";
+
+  std::cout << "This test currently does not get the numbers from the loadScidac function.\n";
+  std::cout << "The real version should perform an actual comparison, instead of relying on a user to check.\n";
   return 0;
 }

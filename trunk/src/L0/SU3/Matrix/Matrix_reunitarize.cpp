3fa98f131efa141a873e6c84e7183ed6f836ae73
Albert.Deuzeman <Albert.Deuzeman@34a3b8a4-774f-0410-9567-b5325461a675>
2010-09-07 11:39:52 +0000

    Added missing restriction from U(3) to SU(3).

--- trunk/src/L0/SU3/Matrix/Matrix_reunitarize.cpp
+++ trunk/src/L0/SU3/Matrix/Matrix_reunitarize.cpp
@@ -7,18 +7,20 @@
  * *********************************************************/
 
 void SU3::Matrix::reunitarize() 
-{ double check=10;
-  int j=0;
+{ 
+  double check;
   do
   {
     Matrix X_j = *this;
     Matrix Xbuff = X_j.inverse();
     double gamma = sqrt(Xbuff.norm()/Xbuff.norm());
-    operator*=(0.5 * gamma);  operator+=((0.5/gamma) * Xbuff.dagger());
+    operator*=(0.5 * gamma);
+    operator+=((0.5 / gamma) * Xbuff.dagger());
     X_j -= *this;
-    check = sqrt(X_j.norm());  j++;
+    check = sqrt(X_j.norm());
   }
-  while(check > 1e-15);//std::cout << "j" << j << std::endl;
+  while(check > 1e-15);
+  operator/=(std::pow(det(), 1/3));
 }
 
 

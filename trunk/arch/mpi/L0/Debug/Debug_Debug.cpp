09832f2f340194a0be91868b9b360a915e236b51
siebren.reker <siebren.reker@34a3b8a4-774f-0410-9567-b5325461a675>
2010-09-07 15:46:14 +0000

    Added a Debugging class, that should help provide output in a unified way. Independent of MPI or Scalar architecture, please test and use.
    Reinserted the MPIstartstop test, since it now only builds in MPI builds.
    
    Minor textual change in reunitarize (whitespace)
    
    tests and examples will now only be built from within their respective directories

---
+++ trunk/arch/mpi/L0/Debug/Debug_Debug.cpp
@@ -0,0 +1,6 @@
+#include "Debug.ih"
+
+Debug::Debug(std::string dbgstr)
+{
+  std::cout << "[DEBUG] Node " << MPI::COMM_WORLD.Get_rank() << ": " << dbgstr << std::endl << std::flush;
+}
--- trunk/test/MPIstartstop.cpp
+++ trunk/test/MPIstartstop.cpp
@@ -5,12 +5,12 @@
 int main(int argc, char **argv)
 {
   bool c0 = (MPI::Is_initialized() || MPI::Is_finalized());
-  std::cout << "Before MPI::Init, MPI::Is_initialized is false and MPI::Is_finalized is false... " << (c0 ? "fail\n" : "pass\n");
+  std::cout << "Before MPI::Init,     MPI::Is_initialized is false and MPI::Is_finalized is false... " << (c0 ? "fail\n" : "pass\n");
   MPI::Init(argc, argv);
   bool c1 = (!MPI::Is_initialized() || MPI::Is_finalized());
-  std::cout << "After MPI::Init, MPI::Is_initialized is true and MPI::Is_finalized is false... " << (c1 ? "fail\n" : "pass\n");
+  std::cout << " After MPI::Init,     MPI::Is_initialized is true and MPI::Is_finalized is false... " << (c1 ? "fail\n" : "pass\n");
   MPI::Finalize();
   bool c2 = (!MPI::Is_initialized() || !MPI::Is_finalized());
-  std::cout << "After MPI::Finalize, MPI::Is_initialized is true and MPI::Is_finalized is true... " << (c2 ? "fail\n" : "pass\n");
+  std::cout << " After MPI::Finalize, MPI::Is_initialized is true and MPI::Is_finalized is true... " << (c2 ? "fail\n" : "pass\n");
   return (c0 || c1 || c2);
 }

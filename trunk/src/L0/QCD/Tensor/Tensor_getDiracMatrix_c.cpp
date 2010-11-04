db2063a490876995309662a07c30d3274ef6dd63
fr.sanfilippo <fr.sanfilippo@34a3b8a4-774f-0410-9567-b5325461a675>
2010-11-04 00:18:08 +0000

    OK! This commit fix the previuos not-working one. Now the two points functions are correctly calculated again.

--- trunk/src/L0/QCD/Tensor/Tensor_getDiracMatrix_c.cpp
+++ trunk/src/L0/QCD/Tensor/Tensor_getDiracMatrix_c.cpp
@@ -2,7 +2,7 @@
 
 namespace QCD
 {
-  void getDiracMatrix(Dirac::Matrix &dMatrix, Tensor const &A, QCD::Tensor const &B, bool const colourDilutedSource)
+  void getSpinDilutedDiracMatrix(Dirac::Matrix &dMatrix, Tensor const &A, QCD::Tensor const &B)
   {
       /*  For a white source propagator we can use a faster constructor
       since we know which entries of the tensors are zero.
@@ -11,35 +11,27 @@ namespace QCD
       of the one-end-trick.
     */
 
-    if (!colourDilutedSource)
-    {
-      // note that "RED" as the "source" index actually addresses the only non-zero entries
-      Dirac::Matrix xrA;
-      Dirac::Matrix xgA;
-      Dirac::Matrix xbA;
-      Dirac::Matrix xrB;
-      Dirac::Matrix xgB;
-      Dirac::Matrix xbB;
-
-      A.getDiracMatrix(xrA, Base::col_RED,   Base::col_RED);
-      A.getDiracMatrix(xgA, Base::col_RED,   Base::col_GREEN);
-      A.getDiracMatrix(xbA, Base::col_RED,   Base::col_BLUE);
-
-      B.getDiracMatrix(xrB, Base::col_RED,   Base::col_RED);
-      B.getDiracMatrix(xgB, Base::col_GREEN, Base::col_RED);
-      B.getDiracMatrix(xbB, Base::col_BLUE,  Base::col_RED);
-
-      // note that second QCD::Tensor is supposed to appear as a
-      // hermitian conjugate one, so source and sink indices are swapped
-      xrA *= xrB;
-      xgA *= xgB;
-      xbA *= xbB;
-
-      xrA += xgA;
-      xrA += xbA;
-      std::copy(&(xrA[0]), &(xrA[0]) + 16, &(dMatrix[0]));
-    }
-    else if (colourDilutedSource)
-      getDiracMatrix(dMatrix, A, B);
-  }
+    Dirac::Matrix xrA;
+    Dirac::Matrix xgA;
+    Dirac::Matrix xbA;
+    Dirac::Matrix xrB;
+    Dirac::Matrix xgB;
+    Dirac::Matrix xbB;
+    
+    A.getDiracMatrix(xrA, Base::col_RED,   Base::col_RED);
+    A.getDiracMatrix(xgA, Base::col_RED,   Base::col_GREEN);
+    A.getDiracMatrix(xbA, Base::col_RED,   Base::col_BLUE);
+    
+    B.getDiracMatrix(xrB, Base::col_RED,   Base::col_RED);
+    B.getDiracMatrix(xgB, Base::col_RED,   Base::col_GREEN);
+    B.getDiracMatrix(xbB, Base::col_RED,   Base::col_BLUE);
+    
+    xrA *= xrB;
+    xgA *= xgB;
+    xbA *= xbB;
+    
+    xrA += xgA;
+    xrA += xbA;
+    std::copy(&(xrA[0]), &(xrA[0]) + 16, &(dMatrix[0]));
+  }  
 }

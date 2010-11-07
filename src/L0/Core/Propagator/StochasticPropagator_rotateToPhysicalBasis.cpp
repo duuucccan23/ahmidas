2ca7a267ce9c984e2a49f5214b3320822da730d6
fr.sanfilippo <fr.sanfilippo@34a3b8a4-774f-0410-9567-b5325461a675>
2010-11-07 20:04:33 +0000

    added forgotten files

---
+++ src/L0/Core/Propagator/StochasticPropagator_rotateToPhysicalBasis.cpp
@@ -0,0 +1,67 @@
+#include "Propagator.ih"
+
+//multply only on sink by (1+-g5)/sqrt(2)
+
+namespace Core
+{
+  template< >
+  void StochasticPropagator <1> ::rotateToPhysicalBasis(bool const sign)
+  {
+    isolate();
+
+    Dirac::Gamma< 5 > gamma5;
+
+    StochasticPropagator<1> tmp(*this);
+    tmp.rightMultiply(gamma5);
+    if(sign==0) tmp *= std::complex< double >(0,-1);
+    else tmp *= std::complex< double >(0,+1);
+    (*this) += tmp;
+
+    (*this) *= 1/sqrt(2);
+  }
+
+  template< >
+  void StochasticPropagator <4>::rotateToPhysicalBasis(bool const sign)
+  {
+    isolate();
+
+    Dirac::Gamma< 5 > gamma5;
+
+    StochasticPropagator<4> tmp(*this);
+    tmp *= gamma5;
+    if(sign==0) tmp *= std::complex< double >(0,-1);
+    else tmp *= std::complex< double >(0,+1);
+    (*this) += tmp;
+
+    tmp = (*this);
+    tmp.rightMultiply(gamma5);
+    if(sign==0) tmp *= std::complex< double >(0,-1);
+    else tmp *= std::complex< double >(0,+1);
+    (*this) += tmp;
+
+    (*this) *= 0.5; //this is (1/sqrt(2))^2                                                                                  
+  }
+
+  template <>
+  void StochasticPropagator<12>::rotateToPhysicalBasis(bool const sign)
+  {
+    isolate();
+
+    Dirac::Gamma< 5 > gamma5;
+
+    StochasticPropagator<12> tmp(*this);
+    tmp *= gamma5;
+    if(sign==0) tmp *= std::complex< double >(0,-1);
+    else tmp *= std::complex< double >(0,+1);
+    (*this) += tmp;
+
+    tmp = (*this);
+    tmp.rightMultiply(gamma5);
+    if(sign==0) tmp *= std::complex< double >(0,-1);
+    else tmp *= std::complex< double >(0,+1);
+    (*this) += tmp;
+
+    (*this) *= 0.5; //this is (1/sqrt(2))^2                                                                                  
+  }
+
+}

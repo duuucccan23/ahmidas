6c2574b0f38312d7e78a04243082aa6e949274ce
dinter <dinter@ifh.de>
2011-01-06 12:28:00 +0000

    Renaming of some variables.

--- CMakeModules/FindLEMON.cmake
+++ CMakeModules/FindLEMON.cmake
@@ -2,23 +2,23 @@
 # Once done, this will define
 #
 #  LEMON_FOUND - system has Lemon
-#  LEMON_INCLUDES - the Lemon include directories
-#  LEMON_LIBRARIES - link these to use Lemon
+#  LEMON_INCLUDE_PATH - the Lemon include directories
+#  LEMON_LIBRARY - link these to use Lemon
 
 # Find the include directory
-FIND_PATH(LEMON_INCLUDES NAMES lemon.h PATHS /usr/include /usr/local/include ${CMAKE_INCLUDE_PATH} ${CMAKE_PREFIX_PATH}/include $ENV{LEMONDIR}/include PATH_SUFFIXES lemon)
+FIND_PATH(LEMON_INCLUDE_PATH NAMES lemon.h PATHS /usr/include /usr/local/include ${CMAKE_INCLUDE_PATH} ${CMAKE_PREFIX_PATH}/include $ENV{LEMONDIR}/include PATH_SUFFIXES lemon)
 
 # Find the library itself
-FIND_LIBRARY(LEMON_LIBRARIES NAMES liblemon.a PATHS /usr/lib /usr/local/lib ${CMAKE_LIBRARY_PATH} ${CMAKE_PREFIX_PATH}/lib $ENV{LEMONDIR}/lib $ENV{LEMONDIR}/src PATH_SUFFIXES lemon)
+FIND_LIBRARY(LEMON_LIBRARY NAMES liblemon.a PATHS /usr/lib /usr/local/lib ${CMAKE_LIBRARY_PATH} ${CMAKE_PREFIX_PATH}/lib $ENV{LEMONDIR}/lib $ENV{LEMONDIR}/src PATH_SUFFIXES lemon)
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
-IF (LEMON_INCLUDES AND LEMON_LIBRARIES)
+IF (LEMON_INCLUDE_PATH AND LEMON_LIBRARY)
    SET(LEMON_FOUND TRUE)
-ENDIF (LEMON_INCLUDES AND LEMON_LIBRARIES)
+ENDIF (LEMON_INCLUDE_PATH AND LEMON_LIBRARY)
 
 IF (LEMON_FOUND)
    IF (NOT LEMON_FIND_QUIETLY)
-      MESSAGE(STATUS "Found Lemon: ${LEMON_LIBRARIES}")
+      MESSAGE(STATUS "Found Lemon: ${LEMON_LIBRARY}")
    ENDIF (NOT LEMON_FIND_QUIETLY)
 ELSE (LEMON_FOUND)
    IF (LEMON_FIND_REQUIRED)

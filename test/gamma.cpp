5d29f0526664855afbd17f1d0048a2e33fbcb464
drachvincent <drachvincent@34a3b8a4-774f-0410-9567-b5325461a675>
2011-01-20 10:00:07 +0000

    Fix a bug in the generation of volume sources - add some gamma multiplication test

--- test/gamma.cpp
+++ test/gamma.cpp
@@ -25,7 +25,71 @@ int main(int argc, char **argv)
       complex< double >(17, 18), std::complex< double >(19, 20),
       complex< double >(21, 22), std::complex< double >(23, 24)};
 
+  Gamma< 1 > gamma1;
+  Gamma< 2 > gamma2;
+  Gamma< 3 > gamma3;
+  Gamma< 4 > gamma4;
   Gamma< 5 > gamma5;
+  complex <double >  plus_i(0,1);
+  complex <double >  minus_i(0,-1);
+
+
+  QCD::Spinor spinor1(refData);
+  spinor1.leftMultiply(gamma1);
+
+  for (int idx=0; idx<3; idx++)
+  {
+	assert(spinor1[0][idx] == minus_i*refData[3*3+idx]);
+    assert(spinor1[1][idx] == minus_i*refData[2*3+idx]);
+    assert(spinor1[2][idx] == plus_i*refData[1*3+idx]);
+    assert(spinor1[3][idx] == plus_i*refData[0*3+idx]);
+  }
+
+  Print("left multiplication by gamma1 ok");
+
+
+  QCD::Spinor spinor2(refData);
+  spinor2.leftMultiply(gamma2);
+
+  for (int idx=0; idx<3; idx++)
+  {
+	assert(spinor2[0][idx] == -refData[3*3+idx]);
+    assert(spinor2[1][idx] ==  refData[2*3+idx]);
+    assert(spinor2[2][idx] ==  refData[1*3+idx]);
+    assert(spinor2[3][idx] == -refData[0*3+idx]);
+  }
+
+  Print("left multiplication by gamma2 ok");
+
+
+  QCD::Spinor spinor3(refData);
+  spinor3.leftMultiply(gamma3);
+
+  for (int idx=0; idx<3; idx++)
+  {
+	assert(spinor3[0][idx] == minus_i*refData[2*3+idx]);
+    assert(spinor3[1][idx] == plus_i*refData[3*3+idx]);
+    assert(spinor3[2][idx] == plus_i*refData[0*3+idx]);
+    assert(spinor3[3][idx] == minus_i*refData[1*3+idx]);
+  }
+
+  Print("left multiplication by gamma3 ok");
+
+
+  QCD::Spinor spinor4(refData);
+  spinor4.leftMultiply(gamma4);
+
+  for (int idx=0; idx<3; idx++)
+  {
+	assert(spinor4[0][idx] == -refData[2*3+idx]);
+    assert(spinor4[1][idx] == -refData[3*3+idx]);
+    assert(spinor4[2][idx] == -refData[0*3+idx]);
+    assert(spinor4[3][idx] == -refData[1*3+idx]);
+  }
+
+  Print("left multiplication by gamma4 ok");
+
+
 
   QCD::Spinor spinor(refData);
   spinor.leftMultiply(gamma5);
@@ -39,7 +103,35 @@ int main(int argc, char **argv)
     assert(spinor[3][idx] == -refData[3*3+idx]);
   }
 
-  Print("That seems to work.");
+  Print("left multiplication by gamma5 ok");
+
+  QCD::Spinor spinor6(refData);
+  spinor6.leftMultiply(gamma5);
+  spinor6.leftMultiply(gamma1);
+
+
+  QCD::Spinor spinor7(refData);
+  spinor7.leftMultiply(gamma1);
+  spinor7.leftMultiply(gamma5);
+
+
+
+  // loop over color entries
+  for (int idx=0; idx<3; idx++)
+  {
+    assert(spinor6[0][idx] + spinor7[0][idx] ==  0.);
+    assert(spinor6[1][idx] + spinor7[1][idx] ==  0.);
+    assert(spinor6[2][idx] + spinor7[2][idx] ==  0.);
+    assert(spinor6[3][idx] + spinor7[3][idx] ==  0.);
+  }
+
+  Print("left multiplication by {gamma5 ,gamma1} ok");
+
+
+
+
+
+
   return EXIT_SUCCESS;
 }
 

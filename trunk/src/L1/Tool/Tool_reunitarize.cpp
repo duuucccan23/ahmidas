349b3f2b9e82ba3a35449a3809ff480128f899ef
drachvincent <drachvincent@34a3b8a4-774f-0410-9567-b5325461a675>
2011-05-30 13:57:06 +0000

    Correct bugs -> disconnected_loops compile on Vargas

--- trunk/src/L1/Tool/Tool_reunitarize.cpp
+++ trunk/src/L1/Tool/Tool_reunitarize.cpp
@@ -1,5 +1,13 @@
 #include "Tool.ih"
 
+void Tool::reunitarize(Core::Field< SU3::Matrix > *field)
+{
+	  field->isolate();
+	    for (Core::Field< SU3::Matrix >::iterator iter = field->begin(); iter != field->end(); ++iter)
+			    iter->reunitarize();
+}
+
+
 void Tool::reunitarize(Core::Field< QCD::Gauge > *field)
 {
   field->isolate();

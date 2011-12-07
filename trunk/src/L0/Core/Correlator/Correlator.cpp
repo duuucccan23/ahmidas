a48670a9862345560aceb85c7456810223d20ccf
drachvincent <drachvincent@34a3b8a4-774f-0410-9567-b5325461a675>
2011-12-07 13:19:41 +0000

    A little bug fix related to the previous commit

--- trunk/src/L0/Core/Correlator/Correlator.cpp
+++ trunk/src/L0/Core/Correlator/Correlator.cpp
@@ -1,5 +1,7 @@
 #include "Correlator.ih"
 
+class complex12;
+class complex16;
 class complex96;
 class complex192;
 
@@ -9,18 +11,29 @@ Core::Field< int * > * Core::Correlator< complex192 >::s_xRelative = NULL;
 template<  >
 Core::Field< int * > * Core::Correlator< complex96 >::s_xRelative = NULL;
 
+template<  >
+Core::Field< int * > * Core::Correlator< complex16 >::s_xRelative = NULL;
+
+template<  >
+Core::Field< int * > * Core::Correlator< complex12 >::s_xRelative = NULL;
+
 template< >
 Core::Field< int * > * Core::Correlator< Dirac::Matrix >::s_xRelative = NULL;
 
 template< >
 Core::Field< int * > * Core::Correlator< std::complex< double > >::s_xRelative = NULL;
 
-
 template<  >
 Core::Field< size_t > * Core::Correlator< complex192 >::s_timelabel = NULL;
 
 template<  >
+Core::Field< size_t > * Core::Correlator< complex16 >::s_timelabel = NULL;
+
+template<  >
 Core::Field< size_t > * Core::Correlator< complex96 >::s_timelabel = NULL;
+
+template<  >
+Core::Field< size_t > * Core::Correlator< complex12 >::s_timelabel = NULL;
 
 template< >
 Core::Field< size_t > * Core::Correlator< Dirac::Matrix >::s_timelabel = NULL;

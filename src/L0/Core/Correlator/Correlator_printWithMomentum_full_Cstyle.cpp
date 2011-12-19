4105779ab01bf5c238c388f7c42b7bf88ee0fff4
dinter <dinter@ifh.de>
2011-12-19 15:32:19 +0000

    Negligible modification in the C-style output that forces it to give the same result as the C++-style output (up to whitespaces).

--- src/L0/Core/Correlator/Correlator_printWithMomentum_full_Cstyle.cpp
+++ src/L0/Core/Correlator/Correlator_printWithMomentum_full_Cstyle.cpp
@@ -8,25 +8,25 @@ namespace Core
   {
     for (int t = 0; t < int(T()); t++)
     {
-      fprintf(out,"%s %+3d %+2d %+2d %+2d  %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e\n", prefix.c_str(), t,
+      fprintf(out,"%s %2d %+2d %+2d %+2d %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e\n", prefix.c_str(), t,
               momentum[0], momentum[1], momentum[2],
               ((*this)[t])[ 0].real(), ((*this)[t])[ 0].imag(),
               ((*this)[t])[ 1].real(), ((*this)[t])[ 1].imag(),
               ((*this)[t])[ 2].real(), ((*this)[t])[ 2].imag(),
               ((*this)[t])[ 3].real(), ((*this)[t])[ 3].imag()); 
-      fprintf(out,"%s %+3d %+2d %+2d %+2d  %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e\n", prefix.c_str(), t,
+      fprintf(out,"%s %2d %+2d %+2d %+2d %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e\n", prefix.c_str(), t,
               momentum[0], momentum[1], momentum[2],
               ((*this)[t])[ 4].real(), ((*this)[t])[ 4].imag(),
               ((*this)[t])[ 5].real(), ((*this)[t])[ 5].imag(),
               ((*this)[t])[ 6].real(), ((*this)[t])[ 6].imag(),
               ((*this)[t])[ 7].real(), ((*this)[t])[ 7].imag()); 
-      fprintf(out,"%s %+3d %+2d %+2d %+2d  %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e\n", prefix.c_str(), t,
+      fprintf(out,"%s %2d %+2d %+2d %+2d %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e\n", prefix.c_str(), t,
               momentum[0], momentum[1], momentum[2],
              ((*this)[t])[ 8].real(), ((*this)[t])[ 8].imag(),
              ((*this)[t])[ 9].real(), ((*this)[t])[ 9].imag(),
              ((*this)[t])[10].real(), ((*this)[t])[10].imag(),
              ((*this)[t])[11].real(), ((*this)[t])[11].imag()); 
-      fprintf(out,"%s %+3d %+2d %+2d %+2d  %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e %9.17e\n", prefix.c_str(), t,
+      fprintf(out,"%s %2d %+2d %+2d %+2d %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e %+15.8e\n", prefix.c_str(), t,
               momentum[0], momentum[1], momentum[2],
              ((*this)[t])[12].real(), ((*this)[t])[12].imag(),
              ((*this)[t])[13].real(), ((*this)[t])[13].imag(),
